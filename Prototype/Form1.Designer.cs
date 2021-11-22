95133bb2da9d7e97e05e21823ad433e478108dfa
Jogger <beltchikov@gmx.de>
2021-11-22 10:41:34 +0100

    Adds connection logic

--- Prototype/Form1.Designer.cs
+++ Prototype/Form1.Designer.cs
@@ -31,7 +31,7 @@
             this.txtHost = new System.Windows.Forms.TextBox();
             this.lblHost = new System.Windows.Forms.Label();
             this.lblPort = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
             this.lblClientId = new System.Windows.Forms.Label();
             this.txtClientId = new System.Windows.Forms.TextBox();
             this.btnConnect = new System.Windows.Forms.Button();
@@ -40,60 +40,64 @@
             // 
             // txtHost
             // 
-            this.txtHost.Location = new System.Drawing.Point(72, 21);
+            this.txtHost.Location = new System.Drawing.Point(63, 16);
+            this.txtHost.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtHost.Name = "txtHost";
-            this.txtHost.Size = new System.Drawing.Size(143, 27);
+            this.txtHost.Size = new System.Drawing.Size(126, 23);
             this.txtHost.TabIndex = 0;
             this.txtHost.Text = "localhost";
             // 
             // lblHost
             // 
             this.lblHost.AutoSize = true;
-            this.lblHost.Location = new System.Drawing.Point(25, 28);
+            this.lblHost.Location = new System.Drawing.Point(22, 21);
             this.lblHost.Name = "lblHost";
-            this.lblHost.Size = new System.Drawing.Size(40, 20);
+            this.lblHost.Size = new System.Drawing.Size(32, 15);
             this.lblHost.TabIndex = 1;
             this.lblHost.Text = "Host";
             // 
             // lblPort
             // 
             this.lblPort.AutoSize = true;
-            this.lblPort.Location = new System.Drawing.Point(255, 28);
+            this.lblPort.Location = new System.Drawing.Point(223, 21);
             this.lblPort.Name = "lblPort";
-            this.lblPort.Size = new System.Drawing.Size(35, 20);
+            this.lblPort.Size = new System.Drawing.Size(29, 15);
             this.lblPort.TabIndex = 3;
             this.lblPort.Text = "Port";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(302, 21);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(71, 27);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.Text = "4001";
+            this.txtPort.Location = new System.Drawing.Point(264, 16);
+            this.txtPort.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtPort.Name = "textBox1";
+            this.txtPort.Size = new System.Drawing.Size(63, 23);
+            this.txtPort.TabIndex = 2;
+            this.txtPort.Text = "4001";
             // 
             // lblClientId
             // 
             this.lblClientId.AutoSize = true;
-            this.lblClientId.Location = new System.Drawing.Point(421, 28);
+            this.lblClientId.Location = new System.Drawing.Point(368, 21);
             this.lblClientId.Name = "lblClientId";
-            this.lblClientId.Size = new System.Drawing.Size(66, 20);
+            this.lblClientId.Size = new System.Drawing.Size(52, 15);
             this.lblClientId.TabIndex = 5;
             this.lblClientId.Text = "Client ID";
             // 
             // txtClientId
             // 
-            this.txtClientId.Location = new System.Drawing.Point(495, 21);
+            this.txtClientId.Location = new System.Drawing.Point(433, 16);
+            this.txtClientId.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtClientId.Name = "txtClientId";
-            this.txtClientId.Size = new System.Drawing.Size(73, 27);
+            this.txtClientId.Size = new System.Drawing.Size(64, 23);
             this.txtClientId.TabIndex = 4;
             this.txtClientId.Text = "1";
             // 
             // btnConnect
             // 
-            this.btnConnect.Location = new System.Drawing.Point(609, 20);
+            this.btnConnect.Location = new System.Drawing.Point(533, 15);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(94, 29);
+            this.btnConnect.Size = new System.Drawing.Size(82, 22);
             this.btnConnect.TabIndex = 0;
             this.btnConnect.Text = "Connect";
             this.btnConnect.UseVisualStyleBackColor = true;
@@ -101,28 +105,31 @@
             // 
             // txtMessage
             // 
-            this.txtMessage.Location = new System.Drawing.Point(25, 95);
+            this.txtMessage.Location = new System.Drawing.Point(22, 71);
+            this.txtMessage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtMessage.Multiline = true;
             this.txtMessage.Name = "txtMessage";
             this.txtMessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtMessage.Size = new System.Drawing.Size(812, 225);
+            this.txtMessage.Size = new System.Drawing.Size(711, 170);
             this.txtMessage.TabIndex = 7;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(877, 428);
+            this.ClientSize = new System.Drawing.Size(767, 321);
             this.Controls.Add(this.txtMessage);
             this.Controls.Add(this.btnConnect);
             this.Controls.Add(this.lblClientId);
             this.Controls.Add(this.txtClientId);
             this.Controls.Add(this.lblPort);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtPort);
             this.Controls.Add(this.lblHost);
             this.Controls.Add(this.txtHost);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,7 +140,7 @@
         private System.Windows.Forms.TextBox txtHost;
         private System.Windows.Forms.Label lblHost;
         private System.Windows.Forms.Label lblPort;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtPort;
         private System.Windows.Forms.Label lblClientId;
         private System.Windows.Forms.TextBox txtClientId;
         private System.Windows.Forms.Button btnConnect;
--- Prototype/Form1.cs
+++ Prototype/Form1.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using IBApi;
+using IBSampleApp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,12 +15,50 @@ namespace Prototype
 {
     public partial class Form1 : Form
     {
+        private EReaderMonitorSignal signal;
+        private IBClient ibClient;
+
+
         public Form1()
         {
             InitializeComponent();
+
+            signal = new EReaderMonitorSignal();
+            ibClient = new IBClient(signal);
+
         }
 
         private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string host = txtHost.Text;
+                int port = Int32.Parse(txtPort.Text);
+                int clientId = Int32.Parse(txtClientId.Text);
+
+                ibClient.ClientSocket.eConnect(host, port, ibClient.ClientId);
+
+                // The EReader Thread
+                var reader = new EReader(ibClient.ClientSocket, signal);
+                reader.Start();
+                new Thread(() =>
+                {
+                    while (ibClient.ClientSocket.IsConnected())
+                    {
+                        signal.waitForSignal();
+                        reader.processMsgs();
+                    }
+                })
+                { IsBackground = true }
+                .Start();
+            }
+            catch (Exception ex )
+            {
+                txtMessage.Text += Environment.NewLine + ex.ToString();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
