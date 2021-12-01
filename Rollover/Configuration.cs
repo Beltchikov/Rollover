f5d5eb8d6d072873f95d5918dcc4e408283e0224
Jogger <beltchikov@gmx.de>
2021-12-01 04:33:57 +0100

    Adds class Configuration

---
+++ Rollover/Configuration.cs
@@ -0,0 +1,11 @@
+﻿namespace Rollover
+{
+    public class Configuration
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public int ClientId { get; set; }
+    }
+}
+
+
