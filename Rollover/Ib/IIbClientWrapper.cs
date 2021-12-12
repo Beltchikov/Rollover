440122d4ae0f064f57eef14eb9c98290e787fb0d
Jogger <beltchikov@gmx.de>
2021-12-12 10:27:35 +0100

    Adds class IbClientWrapper

---
+++ Rollover/Ib/IIbClientWrapper.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Rollover.Ib
+{
+    public interface IIbClientWrapper
+    {
+        public event Action<int, int, string, Exception> Error;
+    }
+}
--- Rollover/Ib/IbClientWrapper.cs
+++ Rollover/Ib/IbClientWrapper.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Rollover.Ib
 {
-    public interface IbClientWrapper
+    public class IbClientWrapper : IIbClientWrapper
     {
         public event Action<int, int, string, Exception> Error;
     }
