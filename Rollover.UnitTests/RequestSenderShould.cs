7dbefbd78601bf248f0f394a239351abb51680d3
Jogger <beltchikov@gmx.de>
2021-12-12 10:25:02 +0100

    Intermediate state

---
+++ Rollover.UnitTests/RequestSenderShould.cs
@@ -0,0 +1,17 @@
+﻿using AutoFixture.Xunit2;
+using NSubstitute;
+using Rollover.Ib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Rollover.UnitTests
+{
+    public class RequestSenderShould
+    {
+        
+    }
+}
---
+++ Rollover/Ib/IbClientWrapper.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Rollover.Ib
+{
+    public interface IbClientWrapper
+    {
+        public event Action<int, int, string, Exception> Error;
+    }
+}
