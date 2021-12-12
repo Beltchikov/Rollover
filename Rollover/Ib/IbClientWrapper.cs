66e56b5269614c8417e42d618789832a5d10eaca
Jogger <beltchikov@gmx.de>
2021-12-12 11:26:32 +0100

    Starts implemetation of IbClientWrapper.Connect

--- Rollover/Ib/IbClientWrapper.cs
+++ Rollover/Ib/IbClientWrapper.cs
@@ -1,21 +1,50 @@
-﻿using IBSampleApp.messages;
+﻿using IBApi;
+using IBSampleApp;
+using IBSampleApp.messages;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Rollover.Ib
 {
     public class IbClientWrapper : IIbClientWrapper
     {
+        private EReaderMonitorSignal _signal;
+        private IBClient _ibClient;
+
         public event Action<int, int, string, Exception> Error;
         public event Action<ConnectionStatusMessage> NextValidId;
         public event Action<ManagedAccountsMessage> ManagedAccounts;
 
+        public IbClientWrapper()
+        {
+            _signal = new EReaderMonitorSignal();
+            _ibClient = new IBClient(_signal);
+        }
+
         public void Connect(string host, int port, int clientId)
         {
-            throw new NotImplementedException();
+            _ibClient.ClientSocket.eConnect(host, port, clientId);
+
+            //string host = txtHost.Text;
+            //int port = Int32.Parse(txtPort.Text);
+            //int clientId = Int32.Parse(txtClientId.Text);
+
+            //ibClient.ClientSocket.eConnect(host, port, ibClient.ClientId);
+
+            //// The EReader Thread
+            //var reader = new EReader(ibClient.ClientSocket, signal);
+            //reader.Start();
+            //new Thread(() =>
+            //{
+            //    while (ibClient.ClientSocket.IsConnected())
+            //    {
+            //        signal.waitForSignal();
+            //        reader.processMsgs();
+            //    }
+            //})
+            //{ IsBackground = true }
+            //.Start();
+
+            //EnableControls(true);
         }
     }
 }
