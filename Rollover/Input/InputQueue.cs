d1ddbcd773d3f11a5fda949ca6468dbb2ddf1143
Jogger <beltchikov@gmx.de>
2021-12-18 18:34:54 +0100

    Introduces ConcurrentQueue

--- Rollover/Input/InputQueue.cs
+++ Rollover/Input/InputQueue.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +9,11 @@ namespace Rollover.Input
 {
     public class InputQueue : IInputQueue
     {
-        private readonly Queue<string> _inputQueue;
+        private readonly ConcurrentQueue<string> _inputQueue;
 
         public InputQueue()
         {
-            _inputQueue = new Queue<string>(100);
+            _inputQueue = new ConcurrentQueue<string>();
         }
         
         public void Enqueue(string item)
@@ -22,9 +23,15 @@ namespace Rollover.Input
 
         string IInputQueue.Dequeue()
         {
-            return _inputQueue.Any() 
-                ? _inputQueue.Dequeue()
-                : null;
+            string item;
+            if(_inputQueue.TryDequeue(out item))
+            {
+                return item;
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public int Count()
