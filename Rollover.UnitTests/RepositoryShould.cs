439fb360ab86ec7240d516be43d314854d3ba2f4
Jogger <beltchikov@gmx.de>
2021-12-24 17:17:19 +0100

    Minor improvements

--- Rollover.UnitTests/RepositoryShould.cs
+++ Rollover.UnitTests/RepositoryShould.cs
@@ -1,13 +1,6 @@
 ﻿using AutoFixture.Xunit2;
 using NSubstitute;
 using Rollover.Ib;
-using Rollover.Input;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Rollover.UnitTests
