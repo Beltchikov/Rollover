5abec453e145b459fd68cd4e037692f8ff124f19
Jogger <beltchikov@gmx.de>
2021-12-24 11:38:56 +0100

    Cleans up the code

--- Rollover/Input/InputProcessor.cs
+++ Rollover/Input/InputProcessor.cs
@@ -31,121 +31,26 @@ namespace Rollover.Input
                 case "WaitingForSymbol":
                     var position = _portfolio.PositionBySymbol(input);
                     var symbol = position?.Contract?.Symbol;
-                    if(position == null || symbol == null)
+                    if (position == null || symbol == null)
                     {
                         return new List<string>();
                     }
 
                     if (!_trackedSymbols.SymbolExists(symbol))
                     {
-                        //var contract = new Contract()
-                        //{
-                        //    Symbol = symbol,
-                        //    SecType = "IND",
-                        //    Currency = "USD",
-                        //    Exchange = "GLOBEX"
-                        //};
-
                         State = _reducer.GetState(State, input);
                         repository.ContractDetails(1, position.Contract);
                     }
 
-                    //            var position = portfolio.PositionBySymbol(input);
-                    //            if (position != null)
-                    //            {
-                    //                var symbol = position.Contract.Symbol;
-
-                    //                if (!trackedSymbols.SymbolExists(input))
-                    //                {
-                    //                    // TODO get con id
-                    //                    //ibClient.ClientSocket.reqContractDetails(CONTRACT_DETAILS_ID, contract);
-                    //                    //ibClient.ContractDetails += HandleContractDataMessage;
-
-
-
-                    //                    trackedSymbols.BeginAdd(70100001, symbol, "GLOBEX", "IND", position.Contract.ConId);
-                    //                    //var outputList = new List<string>();
-                    //                    //outputList.Add($"Symbol {input} added");
-                    //                    //outputList.Add($"Tracked symbols:");
-                    //                    //outputList.AddRange(trackedSymbols.AllAsString());
-                    //                    //return outputList;
-                    //                    return new List<string>();
-                    //                }
-                    //                else
-                    //                {
-                    //                    return new List<string> { $"Symbol {input} is already tracked" };
-                    //                }
-
                     return new List<string>();
+
                 case "ContractInfo":
                     State = _reducer.GetState(State, input);
                     return new List<string> { input };
+
                 default:
                     throw new NotImplementedException();
             }
-
-            // if state NullOrWhitespace - state = Connected
-            //else _state = _reducer.GetState(_state, input);
-            // case Connected
-            // case WaitingForSymbol
-
-            return new List<string>();
-
-            //switch (state)
-            //{
-            //    case "Connecting":
-            //        return new List<string> { input };
-            //    case "Connected":
-            //        return new List<string> { input };
-            //    case "WaitingForSymbol":
-            //        if (input == null)
-            //        {
-            //            return new List<string>();
-            //        }
-            //        else if (input.Contains("STATE"))
-            //        {
-            //            return new List<string> { input };
-            //        }
-            //        else if (input == "Enter a symbol to track:")
-            //        {
-            //            return new List<string> { input };
-            //        }
-            //        else
-            //        {
-            //            var position = portfolio.PositionBySymbol(input);
-            //            if (position != null)
-            //            {
-            //                var symbol = position.Contract.Symbol;
-
-            //                if (!trackedSymbols.SymbolExists(input))
-            //                {
-            //                    // TODO get con id
-            //                    //ibClient.ClientSocket.reqContractDetails(CONTRACT_DETAILS_ID, contract);
-            //                    //ibClient.ContractDetails += HandleContractDataMessage;
-
-
-
-            //                    trackedSymbols.BeginAdd(70100001, symbol, "GLOBEX", "IND", position.Contract.ConId);
-            //                    //var outputList = new List<string>();
-            //                    //outputList.Add($"Symbol {input} added");
-            //                    //outputList.Add($"Tracked symbols:");
-            //                    //outputList.AddRange(trackedSymbols.AllAsString());
-            //                    //return outputList;
-            //                    return new List<string>();
-            //                }
-            //                else
-            //                {
-            //                    return new List<string> { $"Symbol {input} is already tracked" };
-            //                }
-            //            }
-            //            else
-            //            {
-            //                return new List<string> { $"Unknown symbol {input}" };
-            //            }
-            //        }
-            //    default:
-            //        throw new NotImplementedException();
-            //}
         }
     }
 }
