66ca792c5ad2130676b7b3a8c77618cba95b538c
Jogger <beltchikov@gmx.de>
2021-12-27 11:56:23 +0100

    Adds test ReturnCorrectParametersForMnq

--- Rollover/Tracking/QueryParametersConverter.cs
+++ Rollover/Tracking/QueryParametersConverter.cs
@@ -1,12 +1,39 @@
-﻿using System;
-
-namespace Rollover.Tracking
+﻿namespace Rollover.Tracking
 {
     public class QueryParametersConverter : IQueryParametersConverter
     {
-        public ITrackedSymbol TrackedSymbolForReqSecDefOptParams(ITrackedSymbol trackedSymbol)
+        public ITrackedSymbol TrackedSymbolForReqSecDefOptParams(ITrackedSymbol derivative)
         {
-            throw new NotImplementedException();
+            if (derivative.Symbol == "MNQ" && derivative.SecType == "FOP")
+            {
+                ITrackedSymbol underlying = CopyTrackedSymbol(derivative);
+                underlying.SecType = "IND";
+                underlying.Exchange = "GLOBEX";
+                return underlying;
+            }
+
+            return derivative;
+        }
+
+        private ITrackedSymbol CopyTrackedSymbol(ITrackedSymbol trackedSymbol)
+        {
+            var copy = new TrackedSymbol();
+
+            copy.ConId = trackedSymbol.ConId;
+            copy.Currency = trackedSymbol.Currency;
+            copy.Exchange = trackedSymbol.Exchange;
+            copy.LocalSymbol = trackedSymbol.LocalSymbol;
+            copy.NextStrike = trackedSymbol.NextStrike;
+
+            copy.OverNextStrike = trackedSymbol.ConId;
+            copy.ReqIdContractDetails = trackedSymbol.ReqIdContractDetails;
+            copy.ReqIdSecDefOptParams = trackedSymbol.ReqIdSecDefOptParams;
+            copy.SecType = trackedSymbol.SecType;
+            copy.Strike = trackedSymbol.Strike;
+
+            copy.Symbol = trackedSymbol.Symbol;
+
+            return copy;
         }
     }
 }
