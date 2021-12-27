87652a7e36d47c61f175c7501f5873e59e122111
Jogger <beltchikov@gmx.de>
2021-12-27 11:11:05 +0100

    Improves UnderlyingConverter

--- Rollover/Ib/UnderlyingConverter.cs
+++ Rollover/Ib/UnderlyingConverter.cs
@@ -1,5 +1,4 @@
 ﻿using IBApi;
-using System.Collections.Generic;
 
 namespace Rollover.Ib
 {
@@ -9,16 +8,42 @@ namespace Rollover.Ib
         {
             if (derivative.Symbol == "MNQ" && derivative.SecType == "FOP")
             {
-                return new Contract
-                {
-                    SecType = "IND",
-                    Symbol = derivative.Symbol,
-                    Currency = derivative.Currency,
-                    Exchange = "GLOBEX"
-                };
+                Contract underlying = CopyContract(derivative);
+                underlying.SecType = "IND";
+                underlying.Exchange = "GLOBEX";
+                return underlying;
             }
 
             return derivative;
+        }
+
+        private Contract CopyContract(Contract contract)
+        {
+            var copy = new Contract();
+
+            copy.ComboLegs = contract.ComboLegs;
+            copy.ComboLegsDescription = contract.ComboLegsDescription;
+            copy.ConId = contract.ConId;
+            copy.Currency = contract.Currency;
+            copy.DeltaNeutralContract = contract.DeltaNeutralContract;
+
+            copy.Exchange = contract.Exchange;
+            copy.IncludeExpired = contract.IncludeExpired;
+            copy.LastTradeDateOrContractMonth = contract.LastTradeDateOrContractMonth;
+            copy.LocalSymbol = contract.LocalSymbol;
+            copy.Multiplier = contract.Multiplier;
+
+            copy.PrimaryExch = contract.PrimaryExch;
+            copy.Right = contract.Right;
+            copy.SecId = contract.SecId;
+            copy.SecIdType = contract.SecIdType;
+            copy.SecType = contract.SecType;
+
+            copy.Strike = contract.Strike;
+            copy.Symbol = contract.Symbol;
+            copy.TradingClass = contract.TradingClass;
+
+            return copy;
         }
     }
 }
