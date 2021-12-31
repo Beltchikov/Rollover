ee880b202de0dd25944a8a4a6c298799f3581cb7
Jogger <beltchikov@gmx.de>
2021-12-31 23:37:26 +0100

    Improves some unit tests

--- Rollover.UnitTests/IbClientWrapperShould.cs
+++ Rollover.UnitTests/IbClientWrapperShould.cs
@@ -1,23 +1,21 @@
-﻿using IBApi;
+﻿using AutoFixture.Xunit2;
+using IBApi;
 using IBSampleApp.messages;
 using NSubstitute;
 using Rollover.Ib;
-using Rollover.Tracking;
 using Xunit;
 
 namespace Rollover.UnitTests
 {
     public class IbClientWrapperShould
     {
-        [Fact]
-        public void CallEnqueueInOnContractDetails()
+        [Theory, AutoNSubstituteData]
+        public void CallEnqueueInOnContractDetails([Frozen] IIbClientQueue inputQueue)
         {
-            var inputQueue = Substitute.For<IIbClientQueue>();
-           
-            var sut = new IbClientWrapper(inputQueue);
-
             var contractDetails = new ContractDetails();
             var contractDetailsMessage = new ContractDetailsMessage(5, contractDetails);
+
+            var sut = new IbClientWrapper(inputQueue);
             sut.OnContractDetails(contractDetailsMessage);
 
             inputQueue.Received().Enqueue(Arg.Any<object>());
