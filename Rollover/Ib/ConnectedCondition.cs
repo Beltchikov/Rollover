afc184bc0e33d3badc4b5d2951e81618c31ab5cb
Jogger <beltchikov@gmx.de>
2021-12-31 10:52:23 +0100

    Reimplements class ConnectedCondition

--- Rollover/Ib/ConnectedCondition.cs
+++ Rollover/Ib/ConnectedCondition.cs
@@ -6,25 +6,51 @@ namespace Rollover.Ib
 {
     public class ConnectedCondition : IConnectedCondition
     {
-        ConcurrentDictionary<int, object> _messageList = new ConcurrentDictionary<int, object>();   
+        ConcurrentDictionary<int, object> _messageList = new ConcurrentDictionary<int, object>();
+
+        bool _managedAccountsAny;
+        bool _isConnected;
+        bool _idMinus1;
+        bool _errorCode21041;
+        bool _marketData;
 
         public void AddMessage(object message)
         {
+            if((message is ManagedAccountsMessage) && (message as ManagedAccountsMessage).ManagedAccounts.Any())
+            {
+                _managedAccountsAny = true;
+            }
+            else if ((message is ConnectionStatusMessage) && (message as ConnectionStatusMessage).IsConnected)
+            {
+                _isConnected = true;
+            }
+            else if ((message is string) && (message as string).Contains("id=-1"))
+            {
+                _idMinus1 = true;
+            }
+            else if ((message is string) && (message as string).Contains("errorCode=2104"))
+            {
+                _errorCode21041 = true;
+            }
+            else if ((message is string) && (message as string).Contains("Market data"))
+            {
+                _marketData = true;
+            }
+
+
             _messageList[_messageList.Count] = message;
         }
 
         public bool IsConnected()
         {
-            bool condition = _messageList.Values.Any(i => i is ManagedAccountsMessage
-                && (i as ManagedAccountsMessage).ManagedAccounts.Any());
-            condition = condition && !_messageList.Values.Any(i => i is ConnectionStatusMessage
-             && (i as ConnectionStatusMessage).IsConnected);
+            //return _managedAccountsAny
+            //    && _isConnected
+            //    && _idMinus1
+            //    && _errorCode21041
+            //    && _marketData;
 
-            condition = condition && _messageList.Values.Any(i => i is string && (i as string).Contains("id=-1"));
-            condition = condition && _messageList.Values.Any(i => i is string && (i as string).Contains("errorCode=2104"));
-            condition = condition && _messageList.Values.Any(i => i is string && (i as string).Contains("Market data"));
-
-            return condition;
+            return _managedAccountsAny
+                && _isConnected;
         }
     }
 }
