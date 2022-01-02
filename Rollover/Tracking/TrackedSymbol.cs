0ef71d428734206348a10718788ac323cae9d7f6
Jogger <beltchikov@gmx.de>
2022-01-02 12:16:09 +0100

    Minor improvements

--- Rollover/Tracking/TrackedSymbol.cs
+++ Rollover/Tracking/TrackedSymbol.cs
@@ -16,7 +16,7 @@
 
         public override string ToString()
         {
-            return $"{Symbol} {NextStrike} {NextButOneStrike}";
+            return $"{LocalSymbol} {NextStrike} {NextButOneStrike}";
         }
     }
 }
