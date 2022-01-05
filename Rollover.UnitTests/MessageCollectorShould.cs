04b2426b3396471eb4ad168143333b0bc95e8ad4
Beltchikov <beltchikov@gmx.de>
2022-01-05 11:23:13 +0100

    Adds test MessageCollectorShould.CallIbClientEConnect

---
+++ Rollover.UnitTests/MessageCollectorShould.cs
@@ -0,0 +1,19 @@
+﻿using AutoFixture.Xunit2;
+using NSubstitute;
+using Rollover.Ib;
+using Xunit;
+
+namespace Rollover.UnitTests
+{
+    public class MessageCollectorShould
+    {
+        [Theory, AutoNSubstituteData]
+        public void CallIbClientEConnect(
+            [Frozen] IIbClientWrapper ibClient,
+            MessageCollector sut)
+        {
+            sut.eConnect(Arg.Any<string>(), Arg.Any<int>(), Arg.Any<int>());
+            ibClient.Received().eConnect(Arg.Any<string>(), Arg.Any<int>(), Arg.Any<int>());
+        }
+    }
+}
