fe7ba5ecceeb45d63cebcca524c73bbf7e18c7b5
Jogger <beltchikov@gmx.de>
2022-01-30 18:00:44 +0100

    Adds some tests

--- Rollover.UnitTests/TrackedSymbolShould.cs
+++ Rollover.UnitTests/TrackedSymbolShould.cs
@@ -37,5 +37,77 @@ namespace Rollover.UnitTests
             var nextStrike = sut.NextStrike(repository, currentStrike);
             Assert.Equal(expectedNextStrike, nextStrike);
         }
+
+        [Theory, AutoNSubstituteData]
+        public void ReturnNextButOneStrike(TrackedSymbol sut)
+        {
+            double currentStrike = 100;
+            double expectedNextStrike = 120;
+            var strikes = new HashSet<double> { 80, 90, 100, 110, 120 };
+
+            var contractDetails = new ContractDetails();
+            contractDetails.Contract
+                = new Contract { LastTradeDateOrContractMonth = "202203" };
+            var contractDetailsMessage = new ContractDetailsMessage(1, contractDetails);
+            List<ContractDetailsMessage> contractDetailsMessages
+                = new List<ContractDetailsMessage> { contractDetailsMessage };
+
+            var repository = Substitute.For<IRepository>();
+            repository.GetStrikes(Arg.Any<Contract>(), Arg.Any<string>())
+                .Returns(strikes);
+            repository.ContractDetails(Arg.Any<Contract>())
+                .Returns(contractDetailsMessages);
+
+            var nextStrike = sut.NextButOneStrike(repository, currentStrike);
+            Assert.Equal(expectedNextStrike, nextStrike);
+        }
+
+        [Theory, AutoNSubstituteData]
+        public void ReturnPreviousStrike(TrackedSymbol sut)
+        {
+            double currentStrike = 100;
+            double expectedPreviousStrike = 90;
+            var strikes = new HashSet<double> { 80, 90, 100, 110, 120 };
+
+            var contractDetails = new ContractDetails();
+            contractDetails.Contract
+                = new Contract { LastTradeDateOrContractMonth = "202203" };
+            var contractDetailsMessage = new ContractDetailsMessage(1, contractDetails);
+            List<ContractDetailsMessage> contractDetailsMessages
+                = new List<ContractDetailsMessage> { contractDetailsMessage };
+
+            var repository = Substitute.For<IRepository>();
+            repository.GetStrikes(Arg.Any<Contract>(), Arg.Any<string>())
+                .Returns(strikes);
+            repository.ContractDetails(Arg.Any<Contract>())
+                .Returns(contractDetailsMessages);
+
+            var nextStrike = sut.PreviousStrike(repository, currentStrike);
+            Assert.Equal(expectedPreviousStrike, nextStrike);
+        }
+
+        [Theory, AutoNSubstituteData]
+        public void ReturnPreviousButOneStrike(TrackedSymbol sut)
+        {
+            double currentStrike = 100;
+            double expectedPreviousStrike = 80;
+            var strikes = new HashSet<double> { 80, 90, 100, 110, 120 };
+
+            var contractDetails = new ContractDetails();
+            contractDetails.Contract
+                = new Contract { LastTradeDateOrContractMonth = "202203" };
+            var contractDetailsMessage = new ContractDetailsMessage(1, contractDetails);
+            List<ContractDetailsMessage> contractDetailsMessages
+                = new List<ContractDetailsMessage> { contractDetailsMessage };
+
+            var repository = Substitute.For<IRepository>();
+            repository.GetStrikes(Arg.Any<Contract>(), Arg.Any<string>())
+                .Returns(strikes);
+            repository.ContractDetails(Arg.Any<Contract>())
+                .Returns(contractDetailsMessages);
+
+            var nextStrike = sut.PreviousButOneStrike(repository, currentStrike);
+            Assert.Equal(expectedPreviousStrike, nextStrike);
+        }
     }
 }
