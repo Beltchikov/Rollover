b8fe0fa3e9c772e130f7048afdf5ff8c044f0a0a
Jogger <beltchikov@gmx.de>
2022-09-20 10:24:36 +0200

    Introduce working connection

--- PrototypeWpf/MainWindow.xaml.cs
+++ PrototypeWpf/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using IBApi;
 using System;
+using System.Linq;
 using System.Threading;
 using System.Windows;
+using System.Windows.Controls;
 using TwsApi;
 
 namespace PrototypeWpf
@@ -13,11 +15,16 @@ namespace PrototypeWpf
     {
         private IBClient _ibClient;
         private EReaderMonitorSignal _signal;
+        private int _nextOrderId;
 
         public MainWindow()
         {
             _signal = new EReaderMonitorSignal();
             _ibClient = new IBClient(_signal);
+
+            _ibClient.NextValidId += _ibClient_NextValidId;
+            _ibClient.Error += _ibClient_Error;
+            _ibClient.ManagedAccounts += _ibClient_ManagedAccounts;
 
             InitializeComponent();
         }
@@ -51,6 +58,52 @@ namespace PrototypeWpf
                     + (String.IsNullOrWhiteSpace(tbMessages.Text)
                         ? String.Empty
                         : Environment.NewLine + tbMessages.Text);
+            }
+        }
+
+        private void _ibClient_NextValidId(TwsApi.messages.ConnectionStatusMessage statusMessage)
+        {
+            _nextOrderId = _ibClient.NextOrderId;
+            string msg = statusMessage.IsConnected
+                ? $"OnNextValidId: Connected! Your client Id: {_ibClient.ClientId}  NextOrderId: {_nextOrderId}"
+                : "nNextValidId: Disconnected...";
+
+            AddLineToTextbox(tbMessages, msg);
+        }
+
+        private void _ibClient_Error(int id, int errorCode, string msg, Exception ex)
+        {
+            AddLineToTextbox(tbMessages, $"OnError: id={id} errorCode={errorCode} msg={msg} exception={ex}");
+        }
+
+        private void _ibClient_ManagedAccounts(TwsApi.messages.ManagedAccountsMessage message)
+        {
+            if (!message.ManagedAccounts.Any())
+            {
+                throw new Exception("OnManagedAccounts: Unexpected");
+            }
+
+            string msg = Environment.NewLine + "OnManagedAccounts: Acounts found: " + message.ManagedAccounts.Aggregate((r, n) => r + ", " + n);
+            AddLineToTextbox(tbMessages, msg);
+        }
+
+        private void AddLineToTextbox(TextBox textBox, string msg)
+        {
+            if (string.IsNullOrWhiteSpace(msg))
+            {
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = msg;
+            }
+            else
+            {
+                var fullMsg = msg + Environment.NewLine + textBox.Text;
+                fullMsg = fullMsg.Replace(Environment.NewLine + Environment.NewLine, Environment.NewLine);
+
+                textBox.Text = fullMsg;
             }
         }
     }
