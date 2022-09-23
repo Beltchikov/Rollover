1a665c919b82f6b01ada73a9eee0ee78759847a5
Jogger <beltchikov@gmx.de>
2022-09-23 18:28:18 +0200

    Adds TwsWebApi project

---
+++ TwsWebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TwsWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "Price")]
+        public double Get()
+        {
+            return 12.4;
+        }
+    }
+}
