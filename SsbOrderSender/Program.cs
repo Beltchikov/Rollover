f92acb73e5347963e3da6e56da9b189bf5e4fa79
Jogger <beltchikov@gmx.de>
2022-09-30 10:32:04 +0200

    Work in progress

--- SsbOrderSender/Program.cs
+++ SsbOrderSender/Program.cs
@@ -1,5 +1,7 @@
 ﻿using IbClient;
+using System.Diagnostics;
 
+const string LOG_FILE = "SsbOrderSender.log";
 const string HOST = "localhost";
 const int PORT = 4001;
 const int CLIENT_ID = 1;
@@ -18,8 +20,8 @@ _ibClient.ConnectAndStartReaderThread(HOST, PORT, CLIENT_ID);
 
 void _ibClient_Error(int id, int errorCode, string msg, Exception ex)
 {
-    string msgError = "OnError: id={id} errorCode={errorCode} msg={msg} exception={ex}";
-    Console.WriteLine(msgError);
+    string msgError = $"OnError: id={id} errorCode={errorCode} msg={msg} exception={ex}{Environment.NewLine}";
+    WithRetry(() => File.AppendAllText(LOG_FILE, msgError), 10, 10);
 }
 
 void _ibClient_NextValidId(IbClient.messages.ConnectionStatusMessage statusMessage)
@@ -31,5 +33,27 @@ void _ibClient_NextValidId(IbClient.messages.ConnectionStatusMessage statusMessa
 }
 
 while (Console.ReadKey().KeyChar.ToString().ToUpper() != "Q") { };
+
+void WithRetry(Action action, int timeout = 1000, int maxAttempts = 10)
+{
+    int attempt = 1;
+    var time = Stopwatch.StartNew();
+    while (time.ElapsedMilliseconds < timeout)
+    {
+        try
+        {
+            action();
+            return;
+        }
+        catch (Exception ex)
+        {
+            attempt++;
+            if(attempt == maxAttempts)
+            {
+                throw new Exception("Stopped retrying after {maxAttempts} attempts", ex);
+            }
+        }
+    }
+}
 
 
