5f04c59cccd2f9e803473a3ce717356227f308bc
Jogger <beltchikov@gmx.de>
2022-10-29 11:40:10 +0200

    Adds test AttachEventHandlers

--- SsbHedger.UnitTests/LogicShould.cs
+++ SsbHedger.UnitTests/LogicShould.cs
@@ -1,11 +1,29 @@
 using AutoFixture.Xunit2;
 using IbClient;
 using NSubstitute;
+using System.Reflection;
 
 namespace SsbHedger.UnitTests
 {
     public class LogicShould
     {
+        [Theory]
+        [InlineData("NextValidId")]
+        [InlineData("Error")]
+        [InlineData("ManagedAccounts")]
+        [InlineData("ContractDetails")]
+        [InlineData("TickPrice")]
+        [InlineData("TickSize")]
+        [InlineData("TickString")]
+        [InlineData("TickOptionCommunication")]
+        public void AttachEventHandlers(string eventName)
+        {
+            var ibClient = IBClient.CreateClient();
+            var sut = new Logic(ibClient);
+            sut.Execute();
+            VerifyDelegateAttachedTo(ibClient, eventName);
+        }
+
         [Theory, AutoNSubstituteData]
         public void CallIbClientConnectAndStartReaderThread(
             [Frozen] IIBClient ibClient,
@@ -18,5 +36,20 @@ namespace SsbHedger.UnitTests
                 Arg.Any<int>());
         }
 
+        private void VerifyDelegateAttachedTo(object objectWithEvent, string eventName)
+        {
+            var allBindings = BindingFlags.IgnoreCase | BindingFlags.Public |
+            BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static;
+
+            var type = objectWithEvent.GetType();
+            var fieldInfo = type.GetField(eventName, allBindings);
+
+            var value = fieldInfo?.GetValue(objectWithEvent);
+
+            var handler = value as Delegate;
+            Assert.NotNull(handler);
+        }
+
     }
+
 }
--- SsbHedger/Logic.cs
+++ SsbHedger/Logic.cs
@@ -18,10 +18,60 @@ namespace SsbHedger
 
         public void Execute()
         {
-           _ibClient.ConnectAndStartReaderThread(
+            _ibClient.NextValidId += _ibClient_NextValidId;
+            _ibClient.Error += _ibClient_Error;
+            _ibClient.ManagedAccounts += _ibClient_ManagedAccounts;
+            _ibClient.ContractDetails += _ibClient_ContractDetails;
+            _ibClient.TickPrice += _ibClient_TickPrice;
+            _ibClient.TickSize += _ibClient_TickSize;
+            _ibClient.TickString += _ibClient_TickString;
+            _ibClient.TickOptionCommunication += _ibClient_TickOptionCommunication;
+
+
+            _ibClient.ConnectAndStartReaderThread(
                        "localhost",
                        4001,
                        1);
+        }
+
+        private void _ibClient_TickOptionCommunication(IbClient.messages.TickOptionMessage obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void _ibClient_TickString(int arg1, int arg2, string arg3)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void _ibClient_TickSize(IbClient.messages.TickSizeMessage obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void _ibClient_TickPrice(IbClient.messages.TickPriceMessage obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void _ibClient_ContractDetails(IbClient.messages.ContractDetailsMessage obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void _ibClient_ManagedAccounts(IbClient.messages.ManagedAccountsMessage obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void _ibClient_Error(int arg1, int arg2, string arg3, Exception arg4)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void _ibClient_NextValidId(IbClient.messages.ConnectionStatusMessage obj)
+        {
+            throw new NotImplementedException();
         }
     }
 }
