f46cfa18df1f461bd3bc956971a7e34af15e0a0b
Jogger <beltchikov@gmx.de>
2022-11-20 11:52:32 +0100

    Implements MessageListBox

--- SsbHedger/Model/MainWindowViewModel.cs
+++ SsbHedger/Model/MainWindowViewModel.cs
@@ -1,21 +1,11 @@
 ﻿using SsbHedger;
-using SsbHedger.Model;
 using System.Collections.ObjectModel;
 
-namespace ViewModel.ListBinding
+namespace SsbHedger.Model
 {
-    public class ListBindingWindowViewModel : NotifyPropertyChangedImplementation
+    public class MainWindowViewModel : NotifyPropertyChangedImplementation
     {
         private ObservableCollection<Message> messages = new ObservableCollection<Message>();
-
-
-        public ListBindingWindowViewModel()
-        {
-            Messages = new ObservableCollection<Message>
-            {
-                new Message() { ReqId = -1, Body = "ddd" }
-            };
-        }
 
         public ObservableCollection<Message> Messages
         {
