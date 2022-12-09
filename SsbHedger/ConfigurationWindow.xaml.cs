d4ea1f92eba5d4ff996400b1bc99c07c30b6d996
Andrei Beltchikov <beltchikov@gmx.de>
2022-12-09 10:30:44 +0100

    Implements closing of window on cancel button

--- SsbHedger/ConfigurationWindow.xaml.cs
+++ SsbHedger/ConfigurationWindow.xaml.cs
@@ -29,5 +29,10 @@ namespace SsbHedger
             e.Cancel = true;
             this.Visibility = Visibility.Hidden;
         }
+
+        private void btCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
