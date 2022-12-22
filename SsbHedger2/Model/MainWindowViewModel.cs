2ac72e89beda5c34025c0b4b157c91856cca6ed9
Andrei Beltchikov <beltchikov@gmx.de>
2022-12-22 10:31:01 +0100

    Adds project SsbHedger2

---
+++ SsbHedger2/Model/MainWindowViewModel.cs
@@ -0,0 +1,75 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace SsbHedger2.Model
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        private ObservableCollection<Message> messages;
+        private string host = "";
+        private int port;
+        private int clientId;
+        private bool connected;
+
+        public MainWindowViewModel()
+        {
+            messages = new ObservableCollection<Message>();
+        }
+
+        public string Host
+        {
+            get => host;
+            set
+            {
+                SetProperty(ref host, value);
+                OnPropertyChanged(nameof(ConnectionMessage));
+            }
+        }
+
+        public int Port
+        {
+            get => port;
+            set
+            {
+                SetProperty(ref port, value);
+                OnPropertyChanged(nameof(ConnectionMessage));
+            }
+        }
+
+        public int ClientId
+        {
+            get => clientId;
+            set
+            {
+                SetProperty(ref clientId, value);
+                OnPropertyChanged(nameof(ConnectionMessage));
+            }
+        }
+
+        public string ConnectionMessage
+        {
+            get
+            {
+                return connected
+                    ? $"CONNECTED! {host}, {port}, client ID: {clientId}"
+                    : $"NOT CONNECTED! {host}, {port}, client ID: {clientId}";
+            }
+        }
+
+        public bool Connected
+        {
+            get => connected;
+            set
+            {
+                SetProperty(ref connected, value);
+                OnPropertyChanged(nameof(ConnectionMessage));
+            }
+        }
+
+        public ObservableCollection<Message> Messages
+        {
+            get => messages;
+            set => SetProperty(ref messages, value);
+        }
+    }
+}
