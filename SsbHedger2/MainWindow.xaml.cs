1ab872b725566f2843a6fa3ef0c1eb81e6e446bf
Andrei Beltchikov <beltchikov@gmx.de>
2022-12-27 12:28:58 +0100

    Improves initializing of ConfigurationWindow

--- SsbHedger2/MainWindow.xaml.cs
+++ SsbHedger2/MainWindow.xaml.cs
@@ -8,21 +8,23 @@ namespace SsbHedger2
     /// </summary>
     public partial class MainWindow : Window
     {
-        ConfigurationWindow _configurationWindow;
+        ConfigurationWindow? _configurationWindow;
 
         public MainWindow()
         {
             InitializeComponent();
-
-            //((MainWindowViewModel)DataContext).Host = host;
-            //((MainWindowViewModel)DataContext).Port = port;
-            //((MainWindowViewModel)DataContext).ClientId = clientId;
-
-            _configurationWindow = new("", 0, 0);
         }
 
         private void btConfiguration_Click(object sender, RoutedEventArgs e)
         {
+            if(_configurationWindow == null)
+            {
+                _configurationWindow = new(
+                    ((MainWindowViewModel)DataContext).Host,
+                    ((MainWindowViewModel)DataContext).Port,
+                    ((MainWindowViewModel)DataContext).ClientId);
+            }
+            
             var configurationChanged = _configurationWindow.ShowDialog();
             if (configurationChanged == true)
             {
