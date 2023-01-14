f016e2c011b131dce27a689d718ed1fef687c29c
Andrei Beltchikov <beltchikov@gmx.de>
2023-01-14 09:08:08 +0100

    Adds test IbDateStringConverterShould

---
+++ SsbHedger.UnitTests/Converters/IbDateStringConverterShould.cs
@@ -0,0 +1,19 @@
+﻿using SsbHedger.Converters;
+using System.Globalization;
+
+namespace SsbHedger.UnitTests.Converters
+{
+    public class IbDateStringConverterShould
+    {
+        [Theory]
+        [InlineData("20230111 10:15:00", "2023.01.11 10:15:00")]
+        public void ConvertCorrectly(string ibDateString, string expectedDateString)
+        {
+            DateTime expectedDate = DateTime.Parse(expectedDateString, new CultureInfo("DE-de"));
+
+            var sut = new IbDateStringConverter();
+            var result = sut.Convert(ibDateString, typeof(DateTime), new object(), CultureInfo.InvariantCulture);
+            Assert.Equal(expectedDate, result);
+        }
+    }
+}
---
+++ SsbHedger/Converters/IbDateStringConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SsbHedger.Converters
+{
+    public class IbDateStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DateTime.ParseExact("20230111 10:15:00", "yyyyMMdd hh:mm:ss", CultureInfo.InvariantCulture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime valueTyped = (DateTime)value;
+            return $"{valueTyped.Year}";
+        }
+    }
+}
