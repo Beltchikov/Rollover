68fc54218d54743c898d268a91fa4c0a10f1672b
Andrei Beltchikov <beltchikov@gmx.de>
2023-01-16 15:46:34 +0000

    Fixes data flow MainWindow - SsbChart

---
+++ SsbHedger.UnitTests/Converters/TimeToDateTimeConverterShould.cs
@@ -0,0 +1,20 @@
+﻿using SsbHedger.Converters;
+using System.Globalization;
+
+namespace SsbHedger.UnitTests.Converters
+{
+    public class TimeToDateTimeConverterShould
+    {
+        [Theory]
+        [InlineData("15:30", "15:30")]
+        [InlineData("22:15", "22:15")]
+        public void ConvertCorrectly(string sessionTimeString, string expectedDateString)
+        {
+            DateTime expectedDate = DateTime.Parse(expectedDateString, new CultureInfo("DE-de"));
+
+            var sut = new TimeToDateTimeConverter();
+            var result = sut.Convert(sessionTimeString, typeof(DateTime), new object(), CultureInfo.InvariantCulture);
+            Assert.Equal(expectedDate, result);
+        }
+    }
+}
--- SsbHedger/Converters/TimeToDateTimeConverter.cs
+++ SsbHedger/Converters/TimeToDateTimeConverter.cs
@@ -8,33 +8,12 @@ namespace SsbHedger.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value == null)
-            {
-                return DateTime.MinValue;
-            }
-
-            return DateTime.ParseExact((string)value, "yyyyMMdd hh:mm:ss", CultureInfo.InvariantCulture);
+            return DateTime.Parse((string)value);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value == null)
-            {
-                return "";
-            }
-
-            DateTime valueTyped = (DateTime)value;
-            return $"{valueTyped.Year}{EnsureTwoDigits(valueTyped.Month)}" +
-                $"{EnsureTwoDigits(valueTyped.Day)}" +
-                $" {EnsureTwoDigits(valueTyped.Hour)}:" +
-                $"{EnsureTwoDigits(valueTyped.Minute)}:" +
-                $"{EnsureTwoDigits(valueTyped.Second)}";
-        }
-
-        private string EnsureTwoDigits(int month)
-        {
-            string result = month.ToString();
-            return result.Length == 2 ? result : "0" + result;
+            throw new NotImplementedException();
         }
     }
 }
