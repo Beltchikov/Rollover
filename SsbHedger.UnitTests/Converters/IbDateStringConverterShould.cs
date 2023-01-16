4393b97be19232ff850215045c17f24230dce57c
Andrei Beltchikov <beltchikov@gmx.de>
2023-01-16 09:56:52 +0000

    Improves binding of SsbChart control

--- SsbHedger.UnitTests/Converters/IbDateStringConverterShould.cs
+++ SsbHedger.UnitTests/Converters/IbDateStringConverterShould.cs
@@ -6,28 +6,30 @@ namespace SsbHedger.UnitTests.Converters
     public class IbDateStringConverterShould
     {
         [Theory]
-        [InlineData("2023.01.11 10:15:00", "20230111 10:15:00")]
-        [InlineData("2023.01.02 01:04:09", "20230102 01:04:09")]
-        public void ConvertCorrectly(string dateString, string expectedIbDateString)
-        {
-            DateTime date = DateTime.Parse(dateString, new CultureInfo("DE-de"));
-
-            var sut = new IbDateStringConverter();
-            var result = sut.Convert(date, typeof(string), new object(), CultureInfo.InvariantCulture);
-            Assert.Equal(expectedIbDateString, result);
-        }
-
-        [Theory]
         [InlineData("20230111 10:15:00", "2023.01.11 10:15:00")]
         [InlineData("20230102 01:04:09", "2023.01.02 01:04:09")]
-        public void ConvertBackCorrectly(string ibDateString, string expectedDateString)
+        public void ConvertCorrectly(string ibDateString, string expectedDateString)
         {
             DateTime expectedDate = DateTime.Parse(expectedDateString, new CultureInfo("DE-de"));
 
             var sut = new IbDateStringConverter();
-            var result = sut.ConvertBack(ibDateString, typeof(DateTime), new object(), CultureInfo.InvariantCulture);
+            var result = sut.Convert(ibDateString, typeof(DateTime), new object(), CultureInfo.InvariantCulture);
             Assert.Equal(expectedDate, result);
         }
+
+        [Theory]
+        [InlineData("2023.01.11 10:15:00", "20230111 10:15:00")]
+        [InlineData("2023.01.02 01:04:09", "20230102 01:04:09")]
+        public void ConvertBackCorrectly(string dateString, string expectedIbDateString)
+        {
+            DateTime date = DateTime.Parse(dateString, new CultureInfo("DE-de"));
+
+            var sut = new IbDateStringConverter();
+            var result = sut.ConvertBack(date, typeof(string), new object(), CultureInfo.InvariantCulture);
+            Assert.Equal(expectedIbDateString, result);
+        }
+
+        
 
     }
 }
--- SsbHedger/Converters/IbDateStringConverter.cs
+++ SsbHedger/Converters/IbDateStringConverter.cs
@@ -8,17 +8,27 @@ namespace SsbHedger.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            if (value == null)
+            {
+                return DateTime.MinValue;
+            }
+
+            return DateTime.ParseExact((string)value, "yyyyMMdd hh:mm:ss", CultureInfo.InvariantCulture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return "";
+            }
+
             DateTime valueTyped = (DateTime)value;
             return $"{valueTyped.Year}{EnsureTwoDigits(valueTyped.Month)}" +
                 $"{EnsureTwoDigits(valueTyped.Day)}" +
                 $" {EnsureTwoDigits(valueTyped.Hour)}:" +
                 $"{EnsureTwoDigits(valueTyped.Minute)}:" +
                 $"{EnsureTwoDigits(valueTyped.Second)}";
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return DateTime.ParseExact((string)value, "yyyyMMdd hh:mm:ss", CultureInfo.InvariantCulture);
         }
 
         private string EnsureTwoDigits(int month)
