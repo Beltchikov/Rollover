336a60750a6433f0c4df38cf31ccd62220015e70
Andrei Beltchikov <beltchikov@gmx.de>
2023-01-17 10:14:32 +0000

    Fixes 12 hours time

--- SsbHedger/Converters/SessionStartConverter.cs
+++ SsbHedger/Converters/SessionStartConverter.cs
@@ -8,7 +8,7 @@ namespace SsbHedger.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return ((DateTime)(value)).ToString("hh:mm");
+            return ((DateTime)(value)).ToString("HH:mm");
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
