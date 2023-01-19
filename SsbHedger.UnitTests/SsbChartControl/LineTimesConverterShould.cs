14da175a8fe41838326c9ad1e643256bb24bbeaa
Andrei Beltchikov <beltchikov@gmx.de>
2023-01-19 10:31:52 +0000

    Introduces LineTimesConverter

---
+++ SsbHedger.UnitTests/SsbChartControl/LineTimesConverterShould.cs
@@ -0,0 +1,12 @@
+﻿namespace SsbHedger.UnitTests.SsbChartControl
+{
+    public class LineTimesConverterShould
+    {
+        [Theory]
+        [InlineData("15:30", "15:30")]
+        public void ConvertCorrectly(string timeString, string expectedTimeString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
