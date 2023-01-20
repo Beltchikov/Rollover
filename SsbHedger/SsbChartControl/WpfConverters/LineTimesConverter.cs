50704f4afe13aed682e97504381df1cb8e6f68f8
Andrei Beltchikov <beltchikov@gmx.de>
2023-01-20 17:41:41 +0000

    Improves LineTimesConverter

--- SsbHedger/SsbChartControl/WpfConverters/LineTimesConverter.cs
+++ SsbHedger/SsbChartControl/WpfConverters/LineTimesConverter.cs
@@ -10,7 +10,21 @@ namespace SsbHedger.SsbChartControl.WpfConverters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return ((List<DateTime>)value).Select(x => x.ToString("HH:mm")).ToList();
+            var resultList = new List<string>();
+
+            foreach (var dateTime in (List<DateTime>)value) 
+            { 
+                if(dateTime == DateTime.MinValue)
+                {
+                    resultList.Add("");
+                }
+                else
+                {
+                    resultList.Add(dateTime.ToString("HH:mm"));
+                }
+            }
+            
+            return resultList.ToList();
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
