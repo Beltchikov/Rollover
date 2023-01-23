f8d6ea59e9a80ef49f9705ff85d385aa403a49c9
Andrei Beltchikov <beltchikov@gmx.de>
2023-01-23 15:30:13 +0000

    Introduces HorizontalGridLineConverter

---
+++ SsbHedger/SsbChartControl/WpfConverters/HorizontalGridLineConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SsbHedger.SsbChartControl.WpfConverters
+{
+    public class HorizontalGridLineConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            // TODO
+            return new Point(400,0);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
