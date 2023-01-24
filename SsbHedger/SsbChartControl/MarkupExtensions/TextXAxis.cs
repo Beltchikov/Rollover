fcf57e5b659b4eb3eb10f25c799f4a56b9415369
Andrei Beltchikov <beltchikov@gmx.de>
2023-01-24 10:53:43 +0000

    Minor improvements

--- SsbHedger/SsbChartControl/MarkupExtensions/TextXAxis.cs
+++ SsbHedger/SsbChartControl/MarkupExtensions/TextXAxis.cs
@@ -14,7 +14,7 @@ namespace SsbHedger.SsbChartControl.MarkupExtensions
 
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
-            FormattedText text = new(
+            FormattedText formattedText = new(
                 Text,
                 CultureInfo.CurrentCulture,
                 FlowDirection.LeftToRight,
@@ -23,7 +23,12 @@ namespace SsbHedger.SsbChartControl.MarkupExtensions
                 Brushes.Black,
                 1);
 
-            return text.BuildGeometry(new Point(0, 0));
+            formattedText.SetFontWeight(FontWeights.Thin);
+            formattedText.SetFontSize(10 * (96.0 / 72.0));
+            formattedText.SetForegroundBrush(Brushes.Black);
+            formattedText.SetFontStyle(FontStyles.Normal);
+
+            return formattedText.BuildGeometry(new Point(0, 0));
         }
     }
 }
