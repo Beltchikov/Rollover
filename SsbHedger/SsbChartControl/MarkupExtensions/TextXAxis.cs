74ce992dce1ef074788c2a92bbdefd55e7a86cc2
Andrei Beltchikov <beltchikov@gmx.de>
2023-01-24 10:33:47 +0000

    Introduces TextXAxis markup extension

---
+++ SsbHedger/SsbChartControl/MarkupExtensions/TextXAxis.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace SsbHedger.SsbChartControl.MarkupExtensions
+{
+    public class TextXAxis : MarkupExtension
+    {
+        public string Text { get; set; }
+
+        public TextXAxis() { Text = ""; }  
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            FormattedText text = new(
+                Text,
+                CultureInfo.CurrentCulture,
+                FlowDirection.LeftToRight,
+                new Typeface("Segoe UI"),
+                12,
+                Brushes.Black,
+                1);
+
+            return text.BuildGeometry(new Point(0, 0));
+        }
+    }
+}
---
+++ SsbHedger/SsbChartControl/WpfConverters/XAxisConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SsbHedger.SsbChartControl.WpfConverters
+{
+    public class XAxisConverter : GridRectConverter, IMultiValueConverter
+    {
+        public new object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            Rect rect = (Rect)base.Convert(values, targetType, parameter, culture);
+            return new Rect(rect.X, -8, rect.Width, rect.Height);
+        }
+
+        public new object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
