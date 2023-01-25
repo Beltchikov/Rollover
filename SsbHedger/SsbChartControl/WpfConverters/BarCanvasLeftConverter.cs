2a5d098db751dd7d5a0135ce517b746fb67230e8
Andrei Beltchikov <beltchikov@gmx.de>
2023-01-25 10:22:26 +0000

    Introduces BarCanvasLeftConverter

---
+++ SsbHedger/SsbChartControl/WpfConverters/BarCanvasLeftConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SsbHedger.SsbChartControl.WpfConverters
+{
+    public class BarCanvasLeftConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            // TODO
+            return 20.0;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
