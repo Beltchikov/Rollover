eeb4a943c95d88345c944977c332d81498f4e3d8
Andrei Beltchikov <beltchikov@gmx.de>
2023-01-25 15:14:36 +0000

    Minor improvements

--- SsbHedger/SsbChartControl/WpfConverters/BarCanvasLeftConverter.cs
+++ SsbHedger/SsbChartControl/WpfConverters/BarCanvasLeftConverter.cs
@@ -10,17 +10,21 @@ namespace SsbHedger.SsbChartControl.WpfConverters
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            //if (values[1].GetType() != typeof(Dictionary<DateTime, bool>))
-            //{
-            //    return new Rect(0, 0, DEFAULT_WIDTH, DEFAULT_HEIGHT);
-            //}
+            if (values[1].GetType() != typeof(Dictionary<DateTime, bool>))
+            {
+                return new Rect(
+                    0,
+                    0,
+                    WpfConvertersConstants.DEFAULT_GRID_WIDTH,
+                    WpfConvertersConstants.DEFAULT_GRID_HEIGHT);
+            }
 
-            //int index = (int)values[0];
+            BarUnderlying bar = (BarUnderlying)values[0];
 
-            //Dictionary<DateTime, bool> lineTimesDictionary = (Dictionary<DateTime, bool>)values[1];
-            //int barWidth = (int)values[2];
-            //int yAxisWidth = (int)values[3];
-            //double controlWidth = (double)values[4];
+            Dictionary<DateTime, bool> lineTimesDictionary = (Dictionary<DateTime, bool>)values[1];
+            int barWidth = (int)values[2];
+            int yAxisWidth = (int)values[3];
+            double controlWidth = (double)values[4];
 
             // TODO
             return 20.0;
