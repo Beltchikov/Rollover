ec4485c3da85b0b2c1a055c8948f8d597b378276
Andrei Beltchikov <beltchikov@gmx.de>
2023-01-26 15:33:57 +0000

    Introduces test class BarPricesConverterShould

--- SsbHedger/SsbChartControl/WpfConverters/BarPricesConverter.cs
+++ SsbHedger/SsbChartControl/WpfConverters/BarPricesConverter.cs
@@ -8,15 +8,18 @@ using System.Windows.Data;
 
 namespace SsbHedger.SsbChartControl.WpfConverters
 {
-    public class BarPricesConverter : IValueConverter
+    public class BarPricesConverter : IMultiValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            var bars = (List<BarUnderlying>)value;
-            throw new NotImplementedException();
+            var bars = (List<BarUnderlying>)values[0];
+            var axisHight = (double)values[1];
+
+            // TODO
+            return new List<string> { "100", "101", "102", "13" };
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
