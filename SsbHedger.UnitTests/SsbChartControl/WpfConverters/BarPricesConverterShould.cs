a1b59f18e9b8083bdcfbb170fb671cd931a0f0e2
Andrei Beltchikov <beltchikov@gmx.de>
2023-01-27 18:20:53 +0000

    Starts implementation of test ConvertCorrectlyHeight150

--- SsbHedger.UnitTests/SsbChartControl/WpfConverters/BarPricesConverterShould.cs
+++ SsbHedger.UnitTests/SsbChartControl/WpfConverters/BarPricesConverterShould.cs
@@ -1,17 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SsbHedger.SsbChartControl;
+using System.Collections;
 
 namespace SsbHedger.UnitTests.SsbChartControl.WpfConverters
 {
     public  class BarPricesConverterShould
     {
-        [Fact]
-        public void ConvertCorrectly()
+        [Theory]
+        [ClassData(typeof(Height150TestData))]
+        public void ConvertCorrectlyHeight150(
+            double width,
+            int bufferInPercent,
+            List<BarUnderlying> bars,
+            List<double> expectedLabels)
         {
+            
+            
             throw new NotImplementedException();
         }
+    }
+
+    internal class Height150TestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            var testBars = new List<BarUnderlying>
+            {
+                new BarUnderlying(DateTime.Now, 186,190, 184, 188),
+                new BarUnderlying(DateTime.Now, 170,175, 165, 166),
+                new BarUnderlying(DateTime.Now, 165,168, 160, 162)
+            };
+            var expectedLabels = new List<double> { 165, 185 };
+
+            yield return new object[]
+            {
+                150,
+                10,
+                testBars,
+                expectedLabels
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
