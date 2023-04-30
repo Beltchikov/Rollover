e6e852e1d289dd4c7a9a8a669593fba2427a84b0
Andrei Beltchikov <beltchikov@gmx.de>
2023-04-30 19:25:13 +0200

    Wip

---
+++ ChartControls/Converters/XValuesConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ChartControls.Converters
+{
+    public class XValuesConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            //throw new NotImplementedException();
+            return 20;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
