5aa1109d5878632ea21b7553395d8912f1d5acd2
Andrei Beltchikov <beltchikov@gmx.de>
2023-05-04 20:19:23 +0200

    Introduces GetDiagramY

--- ChartControls/Utilities/IMathUtility.cs
+++ ChartControls/Utilities/IMathUtility.cs
@@ -4,6 +4,7 @@ namespace ChartControls.Utilities
 {
     public interface IMathUtility
     {
-        double GetDiagramX(double diagramWidthOrLength, List<double> datapoints, int idx, double startOffset, double endOffset);
+        double GetDiagramX(double diagramWidth, List<double> datapoints, int idx, double startOffset, double endOffset);
+        double GetDiagramY(double diagramHeight, List<double> datapoints, int idx, double startOffset, double endOffset);
     }
 }
--- ChartControls/Utilities/MathUtility.cs
+++ ChartControls/Utilities/MathUtility.cs
@@ -13,5 +13,14 @@ namespace ChartControls.Utilities
 
             return idx == 0 ? startOffset* koef : startOffset* koef + koef * scaledDistance;
         }
+
+        public double GetDiagramY(double diagramHeight, List<double> datapoints, int idx, double startOffset, double endOffset)
+        {
+            var range = (datapoints.Max() - datapoints.Min()) + startOffset + endOffset;
+            double koef = diagramHeight / range;
+            var scaledDistance = datapoints[idx] - datapoints[0];
+
+            return idx == 0 ? startOffset * koef : startOffset * koef + koef * scaledDistance;
+        }
     }
 }
