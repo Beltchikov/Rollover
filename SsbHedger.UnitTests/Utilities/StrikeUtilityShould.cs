6d6faebc7e5e822db1904a9661b56b403a89df37
Andrei Beltchikov <beltchikov@gmx.de>
2023-06-16 11:22:42 +0200

    Introduces test ReplaceInvalidStrikeCorrectly

---
+++ SsbHedger.UnitTests/Utilities/StrikeUtilityShould.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SsbHedger.UnitTests.Utilities
+{
+    public class StrikeUtilityShould
+    {
+        [Theory]
+        [InlineData("9.5, 10, 10.5", 10.5, 9.9, 0.5, "9.5, 10, 11")]
+        public void ReplaceInvalidStrikeCorrectly(
+            string inputListString,
+            double strike,
+            double underlyingPrice,
+            double strikesStep,
+            string expectedOutputListString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
