aaa93512b9b25e6858973ab202fd7670eed22949
Andrei Beltchikov <beltchikov@gmx.de>
2023-07-31 19:23:20 +0200

    Minor improvements

--- EventTrader/MainWindow.xaml.cs
+++ EventTrader/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,14 +22,19 @@ namespace EventTrader
     /// </summary>
     public partial class MainWindow : Window
     {
+        WebScraper _webScraper; 
+        
         public MainWindow()
         {
             InitializeComponent();
+
+            _webScraper = new WebScraper();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            double ir = _webScraper.AudInterestRate();
+            MessageBox.Show(ir.ToString(new CultureInfo("DE-de")));
         }
     }
 }
---
+++ EventTrader/WebScraper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EventTrader
+{
+    internal class WebScraper
+    {
+        internal double AudInterestRate()
+        {
+            //throw new NotImplementedException();
+
+            // TODO
+            return 33.44;
+        }
+    }
+}
