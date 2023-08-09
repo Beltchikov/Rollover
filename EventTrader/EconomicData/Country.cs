8dbeda24cbc7c623a90ec71222d4375e74275b66
Andrei Beltchikov <beltchikov@gmx.de>
2023-08-09 19:00:56 +0200

    Extends Data class with the readable name

--- EventTrader/EconomicData/Country.cs
+++ EventTrader/EconomicData/Country.cs
@@ -14,79 +14,79 @@ namespace EventTrader.EconomicData
                 case "US":
                     DataList = new List<Data>
                     {
-                        new Data(DataTypeEnum.IrdUs),
-                        new Data(DataTypeEnum.AverageHourlyEarnings),
-                        new Data(DataTypeEnum.CoreCpiMmUs),
-                        new Data(DataTypeEnum.InitialJoblessClaims),
-                        new Data(DataTypeEnum.AdpNonFarmEmploymentChange),
-                        new Data(DataTypeEnum.NonFarmEmploymentChange)
+                        new Data(DataTypeEnum.IrdUs, "IRD"),
+                        new Data(DataTypeEnum.AverageHourlyEarnings, "Average Hourly Earnings"),
+                        new Data(DataTypeEnum.CoreCpiMmUs, "Core CPI m/m"),
+                        new Data(DataTypeEnum.InitialJoblessClaims, "Initial Jobless Claims"),
+                        new Data(DataTypeEnum.AdpNonFarmEmploymentChange, "ADP Non-Farm Employment Change"),
+                        new Data(DataTypeEnum.NonFarmEmploymentChange, "Non-Farm Employment Change")
                     };
                     break;
                 case "EU":
                     DataList = new List<Data>
                     {
-                        new Data(DataTypeEnum.IrdEu),
-                        new Data(DataTypeEnum.GermanPreliminaryCpiMm),
-                        new Data(DataTypeEnum.CpiFlashEstimateYy),
-                        new Data(DataTypeEnum.UnemploymentRateEu)
+                        new Data(DataTypeEnum.IrdEu, "IRD"),
+                        new Data(DataTypeEnum.GermanPreliminaryCpiMm, "German Preliminary CPI m/m"),
+                        new Data(DataTypeEnum.CpiFlashEstimateYy, "CPI Flash Estimate y/y"),
+                        new Data(DataTypeEnum.UnemploymentRateEu, "Unemployment Rate")
                     };
                     break;
                 case "JP":
                     DataList = new List<Data>
                     {
-                        new Data(DataTypeEnum.IrdJp),
-                        new Data(DataTypeEnum.TokioCoreCpiYy),
-                        new Data(DataTypeEnum.NationalCoreCpiYyJp),
-                        new Data(DataTypeEnum.UnemploymentRateJp)
+                        new Data(DataTypeEnum.IrdJp, "IRD"),
+                        new Data(DataTypeEnum.TokioCoreCpiYy, "Tokio Core CPI y/y"),
+                        new Data(DataTypeEnum.NationalCoreCpiYyJp, "National Core CPI y/y"),
+                        new Data(DataTypeEnum.UnemploymentRateJp, "Unemployment Rate")
                     };
                     break;
                 case "GB":
                     DataList = new List<Data>
                     {
-                        new Data(DataTypeEnum.IrdGb),
-                        new Data(DataTypeEnum.CpiYyGb),
-                        new Data(DataTypeEnum.ClaimantCountChange)
+                        new Data(DataTypeEnum.IrdGb, "IRD"),
+                        new Data(DataTypeEnum.CpiYyGb, "CPI y/y"),
+                        new Data(DataTypeEnum.ClaimantCountChange, "Claimant Count Change")
                     };
                     break;
 
                 case "CA":
                     DataList = new List<Data>
                     {
-                        new Data(DataTypeEnum.IrdCa),
-                        new Data(DataTypeEnum.CpiMmCa),
-                        new Data(DataTypeEnum.MedianCpiYyCa),
-                        new Data(DataTypeEnum.TrimmedCpiYyCa),
-                        new Data(DataTypeEnum.UnemploymentRateCa),
+                        new Data(DataTypeEnum.IrdCa, "IRD"),
+                        new Data(DataTypeEnum.CpiMmCa, "CPI m/m"),
+                        new Data(DataTypeEnum.MedianCpiYyCa, "Median CPI y/y"),
+                        new Data(DataTypeEnum.TrimmedCpiYyCa, "Trimmed CPI y/y"),
+                        new Data(DataTypeEnum.UnemploymentRateCa, "Unemployment Rate"),
                     };
                     break;
                 case "AU":
                     DataList = new List<Data>
                     {
-                        new Data(DataTypeEnum.IrdAu),
-                        new Data(DataTypeEnum.CommodityPricesYy),
-                        new Data(DataTypeEnum.MiInflationGaugeMm),
-                        new Data(DataTypeEnum.CpiYyAu),
-                        new Data(DataTypeEnum.CpiQqAu),
-                        new Data(DataTypeEnum.TrimmedMeanCpiQqAu),
-                        new Data(DataTypeEnum.UnemploymentRateAu)
+                        new Data(DataTypeEnum.IrdAu, "IRD"),
+                        new Data(DataTypeEnum.CommodityPricesYy, "Commodity Prices y/y"),
+                        new Data(DataTypeEnum.MiInflationGaugeMm, "MI Inflation Gauge m/m"),
+                        new Data(DataTypeEnum.CpiYyAu, "CPI y/y"),
+                        new Data(DataTypeEnum.CpiQqAu, "CPI q/q"),
+                        new Data(DataTypeEnum.TrimmedMeanCpiQqAu, "Trimmed Mean CPI q/q"),
+                        new Data(DataTypeEnum.UnemploymentRateAu, "Unemployment Rate")
                     };
                     break;
                 case "CH":
                     DataList = new List<Data>
                     {
-                        new Data(DataTypeEnum.IrdCh),
-                        new Data(DataTypeEnum.CpiMmCh),
-                        new Data(DataTypeEnum.UnemploymentRateCh)
+                        new Data(DataTypeEnum.IrdCh, "IRD"),
+                        new Data(DataTypeEnum.CpiMmCh, "CPI m/m"),
+                        new Data(DataTypeEnum.UnemploymentRateCh, "Unemployment Rate")
                     };
                     break;
                 case "NZ":
                     DataList = new List<Data>
                     {
-                        new Data(DataTypeEnum.IrdNz),
-                        new Data(DataTypeEnum.NationalCoreCpiYyNz),
-                        new Data(DataTypeEnum.CpiQqNz),
-                        new Data(DataTypeEnum.GdtPriceIndex),
-                        new Data(DataTypeEnum.UnemploymentRateNz)
+                        new Data(DataTypeEnum.IrdNz, "IRD"),
+                        new Data(DataTypeEnum.NationalCoreCpiYyNz, "National Core CPI y/y"),
+                        new Data(DataTypeEnum.CpiQqNz, "CPI q/q"),
+                        new Data(DataTypeEnum.GdtPriceIndex, "GDT Price Index"),
+                        new Data(DataTypeEnum.UnemploymentRateNz, "Unemployment Rate")
                     };
                     break;
 
--- EventTrader/EconomicData/Data.cs
+++ EventTrader/EconomicData/Data.cs
@@ -2,17 +2,14 @@
 {
     public class Data
     {
-        private Data()
-        {
-                
-        }
-
-        public Data(DataTypeEnum type)
+        public Data(DataTypeEnum type, string name)
         {
             Type = type;
+            Name = name;
         }
 
         public DataTypeEnum Type { get; private set; }  
+        public string Name { get; private set; }  
         public double Actual { get; set; }  
         public double Expected { get; set; }  
         public double Previous { get; set; }  
