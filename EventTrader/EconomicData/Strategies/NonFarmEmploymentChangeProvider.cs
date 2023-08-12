b9a16e673b2ae9d5b5a33c9cf432343000f553db
Andrei Beltchikov <beltchikov@gmx.de>
2023-08-12 14:05:53 +0200

    Improves NonFarmEmploymentChangeProvider

--- EventTrader/EconomicData/Strategies/NonFarmEmploymentChangeProvider.cs
+++ EventTrader/EconomicData/Strategies/NonFarmEmploymentChangeProvider.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Globalization;
 using System.Xml;
+using System.Xml.Linq;
 using System.Xml.XPath;
 
 namespace EventTrader.EconomicData.Strategies
@@ -11,7 +12,10 @@ namespace EventTrader.EconomicData.Strategies
         IBrowserWrapper _browserWrapper;
         XmlNamespaceManager _xmlNamespaceManager;
         const string URL = "https://www.investing.com/economic-calendar/";
-        const string XPATH = "//*[@id=\"eventRowId_479304\"]";
+        const string XPATH_ACTUAL = "//*[@id=\"eventActual_479390\"]";
+        const string XPATH_EXPECTED = "//*[@id=\"eventForecast_479390\"]";
+        const string XPATH_PREVIOUS = "//*[@id=\"eventPrevious_479390\"]";
+        const string NULL_PLACEHOLDER = "&nbsp;";
 
         public NonFarmEmploymentChangeProvider(IBrowserWrapper browserWrapper)
         {
@@ -29,23 +33,38 @@ namespace EventTrader.EconomicData.Strategies
 
             var xDocument = _browserWrapper.XDocument;
 
-            var xElement = xDocument.XPathSelectElement(XPATH, _xmlNamespaceManager)
-                ?? throw new ApplicationException($"Can not find XPath element: {XPATH}");
-            var xElementValue = xElement.Value
-                ?? throw new ApplicationException($"The value of the XElement is null.");
-            var values = xElementValue.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            
-            var actual = ParseDouble(values[6][..^1]);
-            var expected = ParseDouble(values[7][..^1]);
-            var previous = ParseDouble(values[8][..^1]);
+            double? actual = GetDoubleValueByXPath(xDocument, XPATH_ACTUAL);
+            double? expected = GetDoubleValueByXPath(xDocument, XPATH_EXPECTED);
+            double? previous = GetDoubleValueByXPath(xDocument, XPATH_PREVIOUS);
 
             return (actual, expected, previous);
         }
 
-        private double ParseDouble(string doubleAsText)
+        private double? GetDoubleValueByXPath(XDocument xDocument, string xPath)
         {
-            double doubleValue;
-            if (!Double.TryParse(doubleAsText, NumberStyles.Currency, CultureInfo.InvariantCulture, out doubleValue))
+            double? actual;
+            var xElementActual = xDocument.XPathSelectElement(xPath, _xmlNamespaceManager)
+                            ?? throw new ApplicationException($"Can not find XPath element: {xPath}");
+            var stringValueActual = xElementActual.Value.Trim()
+                ?? throw new ApplicationException($"The value of the XElement is null.");
+            actual = ParseDouble(stringValueActual, NULL_PLACEHOLDER, ParseOptions.RemoveLastCharacter);
+            return actual;
+        }
+
+        private double? ParseDouble(string doubleAsText, string nullPlaceholder, ParseOptions parseOptions)
+        {
+            if(doubleAsText.Trim().ToLower() == nullPlaceholder.Trim().ToLower())
+            {
+                return null;
+            }
+
+            string preProcessed = doubleAsText;  
+            if (parseOptions == ParseOptions.RemoveLastCharacter) 
+            {
+                preProcessed = preProcessed[..^1];
+            }
+
+            if (!double.TryParse(preProcessed, NumberStyles.Currency, CultureInfo.InvariantCulture, out double doubleValue))
             {
                 throw new ApplicationException($"Can not parse {doubleAsText} into double.");
             }
