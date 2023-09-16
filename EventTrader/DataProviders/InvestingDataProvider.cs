01e01633e3da0502bb2fcdb86ccc6cf8842abac6
Andrei Beltchikov <beltchikov@gmx.de>
2023-09-16 16:20:53 +0200

    wip

--- EventTrader/DataProviders/InvestingDataProvider.cs
+++ EventTrader/DataProviders/InvestingDataProvider.cs
@@ -1,13 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using Dsmn.WebScraping;
+using System.Collections.Generic;
 using System.Windows;
 
 namespace Dsmn.DataProviders
 {
     public class InvestingDataProvider : IInvestingDataProvider
     {
+        IBrowserWrapper _browserWrapper;
+
+        public InvestingDataProvider(IBrowserWrapper browserWrapper)
+        {
+            _browserWrapper = browserWrapper;
+        }
         public List<string> ExpectedEps(List<string> tickerList)
         {
-            // TODO
+            //foreach (string ticker in tickerList)
+            //{
+
+            //}
+            
+            
+            //TODO
             MessageBox.Show("ExpectedEps");
             return tickerList;
         }
