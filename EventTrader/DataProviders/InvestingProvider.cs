4e3557bcad9c281450742878edf737e05d61e9dc
Andrei Beltchikov <beltchikov@gmx.de>
2023-09-25 12:11:43 +0200

    Prepares XDocument.Parse in InvestingProvider

--- EventTrader/DataProviders/InvestingProvider.cs
+++ EventTrader/DataProviders/InvestingProvider.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections.Generic;
 using System.Windows;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace Dsmn.DataProviders
 {
     public class InvestingProvider : IInvestingProvider
     {
+        XmlNamespaceManager _xmlNamespaceManager;
+
+        public InvestingProvider()
+        {
+            _xmlNamespaceManager = new XmlNamespaceManager(new NameTable());
+            _xmlNamespaceManager.AddNamespace("empty", "http://bel.com/2023/bel-schema");
+        }
+
         public List<string> GetEarningsData(string htmlSource)
         {
             var result = new List<string>();
+
+            htmlSource = htmlSource.Replace("/&nbsp;", "");
+            htmlSource = htmlSource.Replace("&nbsp;", "");
+            var xDocument = XDocument.Parse(htmlSource);
 
             MessageBox.Show("GetEarningsData");
 
