c655aa647afc17c0a5a2cf8f0de2c0af6d8d9c99
Andrei Beltchikov <beltchikov@gmx.de>
2023-09-28 12:01:21 +0200

    Introduces DecimalSeparator user contraol

---
+++ EventTrader/UserConttrols/DecimalSeparator.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Eomn.UserConttrols
+{
+    /// <summary>
+    /// Interaction logic for DecimalSeparator.xaml
+    /// </summary>
+    public partial class DecimalSeparator : UserControl
+    {
+        public DecimalSeparator()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public string Separator { get; set; }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
