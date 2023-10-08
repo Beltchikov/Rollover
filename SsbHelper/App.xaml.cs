d26a075567983dd2f7f2ffbcbe04bea88ec798e4
Andrei Beltchikov <beltchikov@gmx.de>
2023-10-08 17:38:23 +0200

    wip

--- SsbHelper/App.xaml.cs
+++ SsbHelper/App.xaml.cs
@@ -13,5 +13,23 @@ namespace SsbHelper
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow mainWindow = new(); 
+            mainWindow.Show();
+        }
+
+        //private void Application_Startup(object sender, StartupEventArgs e)
+        //{
+        //    MainWindow mainWindow = new()
+        //    {
+        //        //DataContext = new ViewModel(
+        //        //    Services.GetRequiredService<IInvestingProvider>(),
+        //        //    Services.GetRequiredService<IYahooProvider>(),
+        //        //    Services.GetRequiredService<ITwsProvider>(),
+        //        //    Services.GetRequiredService<IIbHost>())
+        //    };
+        //    mainWindow.Show();
+        //}
     }
 }
