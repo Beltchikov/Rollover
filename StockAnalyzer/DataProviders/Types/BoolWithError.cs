89aebde51f54bc2ea94896e43b13ab4a07eb0113
Andrei Beltchikov <beltchikov@gmx.de>
2023-10-23 17:04:16 +0200

    wip

---
+++ StockAnalyzer/DataProviders/Types/BoolWithError.cs
@@ -0,0 +1,8 @@
+﻿namespace StockAnalyzer.DataProviders.Types
+{
+    public class BoolWithError
+    {
+        public bool? Value { get; set; }
+        public string? Error { get; set; }
+    }
+}
---
+++ StockAnalyzer/DataProviders/Types/DateTimeWithError.cs
@@ -0,0 +1,8 @@
+﻿namespace StockAnalyzer.DataProviders.Types
+{
+    public class DateTimeWithError
+    {
+        public bool? Value { get; set; }
+        public string? Error { get; set; }
+    }
+}
