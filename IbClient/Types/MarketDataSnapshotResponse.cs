0c58ed08a670de263ee159b59ef609e4c07808b7
Andrei Beltchikov <beltchikov@gmx.de>
2023-11-06 14:44:51 +0100

    Adds class MarketDataSnapshotResponse

---
+++ IbClient/Types/MarketDataSnapshotResponse.cs
@@ -0,0 +1,19 @@
+﻿using IbClient.messages;
+using System.Collections.Generic;
+
+namespace IbClient.Types
+{
+    public class MarketDataSnapshotResponse
+    {
+        public MarketDataSnapshotResponse()
+        {
+            TickPriceMessages = new List<TickPriceMessage>();
+            TickSizeMessages = new List<TickSizeMessage>();
+        }
+
+        public List<TickPriceMessage> TickPriceMessages { get; set; }
+        public List<TickSizeMessage> TickSizeMessages { get; set; }
+
+        public bool EndOfSnapshot { get; set; }
+    }
+}
