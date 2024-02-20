c6eb50bce493eab4e9f1af4ee557831f73f10b7f
Andrei Beltchikov <beltchikov@gmx.de>
2024-02-20 19:37:40 +0000

    adds EPS (FWD) logic for non headless mode

--- StockAnalyzer/DataProviders/SeekingAlphaProvider.cs
+++ StockAnalyzer/DataProviders/SeekingAlphaProvider.cs
@@ -8,11 +8,14 @@ using System.Xml.Linq;
 using StockAnalyzer.WebScraping;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace StockAnalyzer.DataProviders
 {
     public class SeekingAlphaProvider : BrowserProviderBase, ISeekingAlphaProvider
     {
+        IWebDriver? driver = null;
         public SeekingAlphaProvider(IBrowserWrapper browserWrapper) : base(browserWrapper) { }
         public async Task<IEnumerable<string>> PeersComparison(string ticker, int delay)
         {
@@ -29,20 +32,32 @@ namespace StockAnalyzer.DataProviders
                 var headerUserAgent = $"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0";
 
                 ChromeOptions options = new ChromeOptions();
-                options.AddArgument("--headless=new");
-                IWebDriver driver = new ChromeDriver(options);
+                //options.AddArgument("--headless=new");
+                driver = new ChromeDriver(options);
                 driver.Navigate().GoToUrl(url);
 
-                IWebElement ele = driver.FindElement(By.CssSelector("li:nth-child(1)"));
+                //IWebElement ele = driver.FindElement(By.CssSelector("li:nth-child(1)"));
+                //IWebElement ele = driver.FindElement(By.XPath("//div[text() = 'EPS (FWD)']"));
+                IWebElement ele = WaitUntilElementVisible(By.XPath("//div[text() = 'EPS (FWD)']"));
 
             });
-
-
 
             return result;
         }
 
-
+        public IWebElement WaitUntilElementVisible(By elementLocator, int timeout = 10)
+        {
+            try
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeout));
+                return wait.Until(ExpectedConditions.ElementIsVisible(elementLocator));
+            }
+            catch (NoSuchElementException)
+            {
+                Console.WriteLine("Element with locator: '" + elementLocator + "' was not found.");
+                throw;
+            }
+        }
 
 
     }
