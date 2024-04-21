d7085121e152ad5f38b8a37518a994f44cfaab62
Andrei Beltchikov <beltchikov@gmx.de>
2024-04-21 11:10:39 +0200

    wip

--- PortalOpener/FormMain.cs
+++ PortalOpener/FormMain.cs
@@ -1,5 +1,6 @@
 using PortalOpener.Opener;
 using System.Reflection;
+using System.Text;
 
 namespace PortalOpener
 {
@@ -8,12 +9,36 @@ namespace PortalOpener
         public FormMain()
         {
             InitializeComponent();
+
+            //this.cmbOpener.
+            //cmbOpener.Items.Add("Tokyo");
         }
 
         private void btGo_Click(object sender, EventArgs e)
         {
-            var types = Assembly.GetExecutingAssembly().GetTypes();
-            var openerTypes = types.Where(t => t.GetInterfaces().Contains(typeof(IOpener)));
+            var executingAssembly = Assembly.GetExecutingAssembly();
+
+            var openerTypes = executingAssembly.GetTypes().Where(t => t.GetInterfaces().Contains(typeof(IOpener)));
+            var openerNames = openerTypes.Select(t => t.FullName).ToList();
+
+            var assemblyName = executingAssembly.FullName;
+            if (assemblyName != null)
+            {
+                var instance = Activator.CreateInstance(assemblyName, openerNames[0]);
+                var unwraped = ((IOpener)instance.Unwrap());
+                unwraped.Execute(new string[1]);
+            }
+            else throw new Exception("Unexpected! assemblyName is null");
+
+            //Object o = Activator.CreateInstance(typeof(StringBuilder));
+
+            //// Append a string into the StringBuilder object and display the
+            //// StringBuilder.
+            //StringBuilder sb = (StringBuilder)o;
+            //sb.Append("Hello, there.");
+            //MessageBox.Show(sb.ToString());
+
+            ///////////////////////////////////////
         }
     }
 }
