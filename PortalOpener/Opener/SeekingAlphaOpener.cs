c57107053678eb390ea60315887c9dc2f1a85d26
Andrei Beltchikov <beltchikov@gmx.de>
2024-04-21 10:27:01 +0200

    wip

---
+++ PortalOpener/Opener/SeekingAlphaOpener.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalOpener.Opener
+{
+    internal class SeekingAlphaOpener : IOpener
+    {
+        public string Execute(string[] symbols)
+        {
+            MessageBox.Show("SeekingAlphaOpener");
+            return "";
+        }
+    }
+}
---
+++ PortalOpener/Opener/TestOpener.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalOpener.Opener
+{
+    internal class TestOpener : IOpener
+    {
+        public string Execute(string[] symbols)
+        {
+            MessageBox.Show("TestOpener");
+            return "";
+        }
+    }
+}
