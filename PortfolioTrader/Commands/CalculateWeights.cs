7b210f738f488ebe280d74e905928f63ca1b8ce6
Jogger <beltchikov@gmx.de>
2024-04-24 04:30:58 +0200

    wip

---
+++ PortfolioTrader/Commands/CalculateWeights.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioTrader.Commands
+{
+    internal class CalculateWeights
+    {
+        public static void Run(ICommandVisitor visitor)
+        {
+
+        }
+    }
+}
