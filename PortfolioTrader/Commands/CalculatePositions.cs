5fa9c1d7a41fb5366c0052c14792ae8dc3e66a40
Jogger <beltchikov@gmx.de>
2024-04-26 04:54:24 +0200

    wip

--- PortfolioTrader/Commands/CalculatePositions.cs
+++ PortfolioTrader/Commands/CalculatePositions.cs
@@ -1,11 +1,5 @@
 ﻿using IBApi;
 using PortfolioTrader.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using TickType = IbClient.Types.TickType;
 
 namespace PortfolioTrader.Commands
@@ -17,13 +11,13 @@ namespace PortfolioTrader.Commands
         public static async Task RunAsync(IBuyConfirmationModelVisitor visitor)
         {
             _visitor = visitor;
-            _visitor.StocksToBuyAsString = await AddPriceColumnAsync();
-            _visitor.StocksToSellAsString = await AddPriceColumnAsync();
+            _visitor.StocksToBuyAsString = await AddPriceColumnAsync(_visitor.StocksToBuyAsString);
+            _visitor.StocksToSellAsString = await AddPriceColumnAsync(_visitor.StocksToSellAsString);
         }
 
-        private static async Task<string> AddPriceColumnAsync()
+        private static async Task<string> AddPriceColumnAsync(string stocksAsString)
         {
-            var stocksToBuyDictionary = SymbolsAndScore.StringToPositionDictionary(_visitor.StocksToBuyAsString);
+            var stocksToBuyDictionary = SymbolsAndScore.StringToPositionDictionary(stocksAsString);
             var resultDictionary = new Dictionary<string, Position>();
             foreach (var kvp in stocksToBuyDictionary)
             {
