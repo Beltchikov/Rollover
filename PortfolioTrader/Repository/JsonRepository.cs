9ac50d5c3c25e6d6ed7181d9ffbea01f2e6fe83f
Andrei Beltchikov <beltchikov@gmx.de>
2024-05-04 15:16:44 +0200

    wip

--- PortfolioTrader/Repository/JsonRepository.cs
+++ PortfolioTrader/Repository/JsonRepository.cs
@@ -11,14 +11,20 @@ namespace PortfolioTrader.Repository
     internal class JsonRepository : IRepository
     {
         private readonly List<KnownSymbol> _knownSymbols;
-        private readonly string JSON_REPOSITORY = "Repository\\KnownSymbols.json";
+        private readonly string KNOWN_SYMBOLS_PATH = "Repository\\KnownSymbols.json";
+
+        private readonly List<string> _notTradeableSymbols;
+        private readonly string NOT_TRADEABLE_SYMBOLS_PATH = "Repository\\NotTradeableSymbols.json";
 
         public JsonRepository()
         {
             string dir = Directory.GetCurrentDirectory();
-            string jsonFilePath = Path.Combine(dir, JSON_REPOSITORY);
-            string knownSymbolsAsString = File.ReadAllText(jsonFilePath);
+            
+            string knownSymbolsAsString = File.ReadAllText(Path.Combine(dir, KNOWN_SYMBOLS_PATH));
             _knownSymbols = JsonSerializer.Deserialize<List<KnownSymbol>>(knownSymbolsAsString)!;
+
+            string notTradeableSymbolsAsString = File.ReadAllText(Path.Combine(dir, NOT_TRADEABLE_SYMBOLS_PATH));
+            _notTradeableSymbols = JsonSerializer.Deserialize<List<string>>(notTradeableSymbolsAsString)!;
         }
 
         public int? GetContractId(string symbol)
