1cea9731c85a79748c185eb7eed7494c128c66c7
Andrei Beltchikov <beltchikov@gmx.de>
2024-05-04 11:46:18 +0200

    wip

--- PortfolioTrader/Repository/JsonRepository.cs
+++ PortfolioTrader/Repository/JsonRepository.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace PortfolioTrader.Repository
 {
     internal class JsonRepository : IRepository
     {
+        private readonly List<KnownSymbol> _knownSymbols;
+        private readonly string JSON_REPOSITORY = "Repository\\KnownSymbols.json";
+
+        public JsonRepository()
+        {
+            string dir = Directory.GetCurrentDirectory();
+            string jsonFilePath = Path.Combine(dir, JSON_REPOSITORY);
+            string knownSymbolsAsString = File.ReadAllText(jsonFilePath);
+            _knownSymbols = JsonSerializer.Deserialize<List<KnownSymbol>>(knownSymbolsAsString)!;
+            //ChatGptBatchResult chatGptBatchResult = JsonSerializer.Deserialize<ChatGptBatchResult>(batch)!;
+        }
+
         public int? GetContractId(string symbol)
         {
-            //todo
-            return null;
+            return _knownSymbols.FirstOrDefault(s=>s.Name == symbol)?.ContractId;
         }
     }
 }
---
+++ PortfolioTrader/Repository/KnownSymbol.cs
@@ -0,0 +1,8 @@
+﻿namespace PortfolioTrader.Repository
+{
+    internal class KnownSymbol
+    {
+        public required string Name { get; set; }    
+        public int ContractId { get; set; }    
+    }
+}
