5c94fbfbbe5d7b9be78769d7fd3ada6a7eedf980
Andrei Beltchikov <beltchikov@gmx.de>
2024-05-07 11:02:56 +0200

    wip

--- PortalOpener/Opener/InvestingOpener.cs
+++ PortalOpener/Opener/InvestingOpener.cs
@@ -12,30 +12,26 @@ namespace PortalOpener.Opener
 {
     internal class InvestingOpener : IOpener
     {
-        private readonly object STRONG_BUY = "Strong Buy";
-        private readonly object BUY = "Buy";
-        private readonly object NEUTRAL = "Neutral";
-        private readonly object SELL = "Sell";
-        private readonly object STRONG_SELL = "Strong Sell";
+        private readonly string URL_INVESTING = "https://www.investing.com/search/?q=";
+        private readonly string STRONG_BUY = "Strong Buy";
+        private readonly string BUY = "Buy";
+        private readonly string NEUTRAL = "Neutral";
+        private readonly string SELL = "Sell";
+        private readonly string STRONG_SELL = "Strong Sell";
 
         public string Execute(string[] symbols)
         {
             var _webDriver = new ChromeDriver();
-            _webDriver.Navigate().GoToUrl("https://www.investing.com/search/?q=tsla");
-           
+            _webDriver.Navigate().GoToUrl(URL_INVESTING + "tsla");
+
             var wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(5));
             var searchSectionMain = wait.Until(x => x.FindElement(By.CssSelector("div.searchSectionMain")));
 
-            //var linkWithEquitiesHref = wait.Until(x => x.FindElement(By.CssSelector("div.searchSectionMain")));
             var linkWithEquitiesHref = searchSectionMain.FindElement(By.CssSelector("a[href^=\"/equities/\"]"));
-            //var linkWithEquitiesHref = searchSectionMain.FindElement(By.CssSelector("a[@href~=\"equities\"]"));
+
 
             var secondUrl = linkWithEquitiesHref.GetAttribute("href");
-
             _webDriver.Navigate().GoToUrl(secondUrl);
-
-            //driver.FindElements(By.XPath("//h1[text()='Table Pagination Demo']"));
-            //WebDriverWait(driver, 15).until_not(lambda driver: driver.find_element(By.XPATH, layovers[0]) and driver.find_element(By.XPATH, layovers[1]))
 
             var divTechnical = wait.Until(x =>
             x.FindElement(By.XPath($"//div[text()='{STRONG_BUY}']")
