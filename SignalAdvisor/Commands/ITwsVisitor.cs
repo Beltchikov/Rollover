9d7efd5ed6244b4629f7b52ff7eb0116ada1ca39
Andrei Beltchikov <beltchikov@gmx.de>
2024-05-11 15:14:17 +0200

    wip

--- SignalAdvisor/Commands/ITwsVisitor.cs
+++ SignalAdvisor/Commands/ITwsVisitor.cs
@@ -1,10 +1,5 @@
 ﻿using IbClient.IbHost;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SignalAdvisor.Commands
 {
