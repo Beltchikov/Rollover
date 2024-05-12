f465f65700b15a4fb2a9fad1337267a5a94a03ef
Andrei Beltchikov <beltchikov@gmx.de>
2024-05-12 17:33:24 +0200

    adds project Signals

---
+++ Signals/Bar.cs
@@ -0,0 +1,11 @@
+﻿namespace Ta
+{
+    public class Bar
+    {
+        double Open {  get; set; }
+        double High {  get; set; }
+        double Low {  get; set; }
+        double Close {  get; set; }
+        DateTime Time {  get; set; }
+    }
+}
---
+++ Signals/Signals.cs
@@ -0,0 +1,10 @@
+﻿namespace Ta
+{
+    public class Signals
+    {
+        public static int InsideUpDown(List<Bar> bars)
+        {
+            return 0; // TODO
+        }
+    }
+}
