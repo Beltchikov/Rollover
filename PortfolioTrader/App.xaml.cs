ffb1bbc1ecd0bdb0f88113b84f8e64f19142af10
Andrei Beltchikov <beltchikov@gmx.de>
2024-05-20 07:50:33 +0200

    changes MAX_BUY_SELL to 40

--- PortfolioTrader/App.xaml.cs
+++ PortfolioTrader/App.xaml.cs
@@ -10,7 +10,7 @@ namespace PortfolioTrader
     public partial class App : Application
     {
         internal static readonly int TIMEOUT = 1000;
-        internal static readonly int MAX_BUY_SELL = 20;
+        internal static readonly int MAX_BUY_SELL = 40;
         internal static readonly string SEC_TYPE_STK = "STK";
         internal static readonly string USD = "USD";
         internal static readonly string EXCHANGE = "SMART";
