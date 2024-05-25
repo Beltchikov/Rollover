6f4c060bc0b7cf8453ed8586e26c941174664179
Andrei Beltchikov <beltchikov@gmx.de>
2024-05-25 14:04:42 +0200

    adds InstrumentControl

---
+++ SignalAdvisor/Model/Instrument.cs
@@ -0,0 +1,8 @@
+﻿namespace SignalAdvisor.Model
+{
+    internal class Instrument
+    {
+        public string Symbol { get; set; }    
+        public bool IsLong { get; set; }    
+    }
+}
---
+++ SignalAdvisor/Model/InstrumentsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace SignalAdvisor.Model
+{
+    internal class InstrumentsViewModel : ObservableObject
+    {
+        private ObservableCollection<Instrument> _instruments = [];
+
+        public ObservableCollection<Instrument> Instruments
+        {
+            get => _instruments;
+            set
+            {
+                SetProperty(ref _instruments, value);
+            }
+        }
+    }
+}
