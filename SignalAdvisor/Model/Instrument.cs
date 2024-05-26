a3f6e18be8b0fccdac250ddc779c7d24a5eb45ea
Andrei Beltchikov <beltchikov@gmx.de>
2024-05-26 08:17:13 +0200

    wip

--- SignalAdvisor/Model/Instrument.cs
+++ SignalAdvisor/Model/Instrument.cs
@@ -5,17 +5,17 @@
         public Instrument()
         {
             Symbol = null!;
-            Signal = null!;
+            LastSignal = null!;
             Algo = null!;
         }
 
         public string Symbol { get; set; }    
         public bool IsLong { get; set; }    
-        public DateTime SignalTime { get; set; }    
-        public string Signal { get; set; }    
+        public DateTime LastSignalTime { get; set; }    
+        public string LastSignal { get; set; }    
         public string Algo { get; set; }    
-        public int SlInCents { get; set; }    
-        public int TpInCents { get; set; }    
-        public int Qty { get; set; }    
+        public double StopLoss { get; set; }    
+        public double TakeProfit { get; set; }    
+        public int Quantity { get; set; }    
     }
 }
