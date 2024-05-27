36352f85e2966c08324243fcab15975ae02100cc
Andrei Beltchikov <beltchikov@gmx.de>
2024-05-27 12:17:21 +0200

    wip

---
+++ SignalAdvisor/Converters/AskPriceConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace SignalAdvisor.Converters
+{
+    public class AskPriceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return "";
+            }
+
+            var valueTyped = (double)value;
+            return valueTyped.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
