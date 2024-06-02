5b4d95003cc53b5e8c94a56a4f4797f13d9fbe1b
Andrei Beltchikov <beltchikov@gmx.de>
2024-06-02 10:17:11 +0200

    wip

--- IbClient.UnitTests/IbHostShould.cs
+++ IbClient.UnitTests/IbHostShould.cs
@@ -28,6 +28,12 @@ namespace IbClient.UnitTests
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             Task.Run(() => {
                 Thread.Sleep(DELAY);
+
+                var noiseResponses = Enumerable.Range(1, 10)
+                .Select(x => CreateOrderStatusMessage(x, x.ToString(), (double)x  + 0.99))
+                .ToList();
+                noiseResponses.ForEach(nr => sut.OrderResponseMapper.AddResponse(nr.OrderId, nr));
+
                 sut.OrderResponseMapper.AddResponse(orderId, filledOrderStatusMessage);
 
             });
