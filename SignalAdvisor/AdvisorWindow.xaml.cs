bef5b6be32859cae39199705f925623fe442cbd5
Andrei Beltchikov <beltchikov@gmx.de>
2024-06-03 09:19:02 +0200

    wip

--- SignalAdvisor/AdvisorWindow.xaml.cs
+++ SignalAdvisor/AdvisorWindow.xaml.cs
@@ -14,14 +14,14 @@ namespace SignalAdvisor
             InitializeComponent();
         }
 
-        private void InstrumentsControl_TradeAction(object sender, TradeActionEventArgs e)
+        private void InstrumentsControl_TradeAction1(object sender, TradeActionEventArgs e)
         {
             var model = (DataContext as AdvisorViewModel) ?? throw new Exception();
             model.InstrumentToTrade = e.Instrument;
             model.SendOrders2StdDevCommand.Execute(model);
         }
 
-        private void InstrumentsControl_TradeNonBracketAction(object sender, TradeActionEventArgs e)
+        private void InstrumentsControl_TradeAction2(object sender, TradeActionEventArgs e)
         {
             var model = (DataContext as AdvisorViewModel) ?? throw new Exception();
             model.InstrumentToTrade = e.Instrument;
