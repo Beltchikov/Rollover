7c6bdd4842f0f0a89dfee2bfd1267a4ed25e572c
Andrei Beltchikov <beltchikov@gmx.de>
2024-07-01 12:27:17 +0200

    wip

--- NpvManager/Commands/AnalystsPrices.cs
+++ NpvManager/Commands/AnalystsPrices.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Threading;
+using System.Windows;
 
 namespace NpvManager.Commands
 {
@@ -7,6 +8,30 @@ namespace NpvManager.Commands
         public static async Task RunAsync(IPositionsVisitor visitor)
         {
             MessageBox.Show("AnalystsPrices!");
+
+            //string fundamentalData = await _ibHost.RequestFundamentalDataAsync(contract, reportType, timeout);
+
+            //public async Task<List<DataStringWithTicker>> FundamentalDataFromContractStrings(List<string> contractStringsTws, string reportType, int timeout)
+            //{
+            //    var result = new List<DataStringWithTicker>();
+
+            //    int cnt = 1;
+            //    foreach (string contractString in contractStringsTws)
+            //    {
+            //        if (string.IsNullOrWhiteSpace(contractString))
+            //        {
+            //            continue;
+            //        }
+
+            //        var contractStringTrimmed = contractString.Trim();
+            //        Contract contract = ContractFromString(contractStringTrimmed);
+            //        TriggerStatus($"Retrieving fundamental data for {contractStringTrimmed}, report type: {reportType} {cnt++}/{contractStringsTws.Count}");
+            //        string fundamentalDataString = await FundamentalDataFromContract(timeout, reportType, contract);
+            //        result.Add(new DataStringWithTicker(contract.Symbol, fundamentalDataString));
+            //    }
+
+            //    return result;
+            //}
         }
     }
 }
