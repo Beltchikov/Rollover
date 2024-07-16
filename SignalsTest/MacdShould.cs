104b31652235c053af6f98000573855006084142
Andrei Beltchikov <beltchikov@gmx.de>
2024-07-16 20:13:37 +0200

    wip

---
+++ SignalsTest/MacdShould.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SignalsTest
+{
+    public class MacdShould
+    {
+        [Fact]
+        void CalculateMacdCorrectly()
+        {
+            //var macd = new  Macd(12, 26, 9, 235.318, 235.233, 0.125);
+        }
+    }
+}
