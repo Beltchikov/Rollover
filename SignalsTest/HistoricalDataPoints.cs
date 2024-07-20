f7d7b6f8970b2e91a289d73290a2e385a04f9c51
Andrei Beltchikov <beltchikov@gmx.de>
2024-07-20 11:23:10 +0200

    wip

---
+++ SignalsTest/HistoricalDataPoints.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using Ta.Indicators;
+
+namespace SignalsTest
+{
+    internal class HistoricalDataPoints : IEnumerable<object[]>
+    {
+        internal record ExpectedValue(double FastEma, double SlowEma, double MacdValue, double Signal);
+
+        internal class ExpectedValues
+        {
+            private List<ExpectedValue> _expectedValues = [];
+
+            internal void Add(ExpectedValue expectedValue)
+            {
+                _expectedValues.Add(expectedValue);
+            }
+        }
+
+        readonly List<object[]> _data = [];
+
+        public HistoricalDataPoints()
+        {
+            var dataPoints = new DataPoints
+            {
+                new DataPoint(DateTimeOffset.Parse("16.07.2024 10:00:00 +01:00"), 234.8),
+                new DataPoint(DateTimeOffset.Parse("16.07.2024 10:05:00 +01:00"), 234.43),
+                new DataPoint(DateTimeOffset.Parse("16.07.2024 10:10:00 +01:00"), 235)
+            };
+
+            var expectedValues = new ExpectedValues();
+            expectedValues.Add(new ExpectedValue(235.318, 235.233, 0.085, 0.125));
+            expectedValues.Add(new ExpectedValue(235.1813846, 235.1735185, 0.007866097, 0.101573219));
+            expectedValues.Add(new ExpectedValue(235.1606653, 235.233, -0.007186005, 0.079821375));
+
+            object[] testRow = [dataPoints, expectedValues];
+            _data.Add(testRow);
+        }
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            return ((IEnumerable<object[]>)_data).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)_data).GetEnumerator();
+        }
+    }
+}
