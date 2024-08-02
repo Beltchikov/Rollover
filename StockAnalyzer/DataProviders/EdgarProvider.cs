3325795bfead8c3d75925ffc9f18acb940cd478d
Andrei Beltchikov <beltchikov@gmx.de>
2024-08-02 18:43:53 +0200

    wip

--- StockAnalyzer/DataProviders/EdgarProvider.cs
+++ StockAnalyzer/DataProviders/EdgarProvider.cs
@@ -1,25 +1,39 @@
 ﻿using StockAnalyzer.DataProviders.Types;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace StockAnalyzer.DataProviders
 {
     internal class EdgarProvider : IEdgarProvider
     {
-        public async Task<IEnumerable<string>> StockholdersEquity(string cik)
+        HttpClient _httpClient = new();
+
+        public EdgarProvider()
         {
-            return await Task.Run(() =>
-            {
-                
-                var liabilitiesAndStockholdersEquity = new LiabilitiesAndStockholdersEquity();
+            _httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("StockAnalyzer/1.0 (beltchikov@gmx.de)");
+        }
 
 
+        public async Task<IEnumerable<string>> StockholdersEquity(string symbol)
+        {
+            string cik = "0000200406"; // TODO
+            string url = $"https://data.sec.gov/api/xbrl/companyconcept/CIK{cik}/us-gaap/LiabilitiesAndStockholdersEquity.json";
+            var response = await _httpClient.GetStringAsync(url);
 
-                return new List<string>() {
+            var liabilitiesAndStockholdersEquity = JsonSerializer.Deserialize<LiabilitiesAndStockholdersEquity>(response);
+
+
+            // TODO
+
+
+            return new List<string>() {
                 "Symbol\t2024-03-31\t2024-06-30",
                 "JNJ\t171966000000\t181088000000",
                 "PG\t10\t18"};
-            });
+
         }
     }
 }
+    
