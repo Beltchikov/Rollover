e249d141ddbbdb8bf836f0c80fa9a1907b5b2c79
Andrei Beltchikov <beltchikov@gmx.de>
2024-08-02 09:44:36 +0200

    wip

--- StockAnalyzer/DataProviders/EdgarProvider.cs
+++ StockAnalyzer/DataProviders/EdgarProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using StockAnalyzer.DataProviders.Types;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace StockAnalyzer.DataProviders
@@ -9,6 +10,11 @@ namespace StockAnalyzer.DataProviders
         {
             return await Task.Run(() =>
             {
+                
+                var liabilitiesAndStockholdersEquity = new LiabilitiesAndStockholdersEquity();
+
+
+
                 return new List<string>() {
                 "Symbol\t2024-03-31\t2024-06-30",
                 "JNJ\t171966000000\t181088000000",
