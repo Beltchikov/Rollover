6876e475f3ad39d00312b30f22b4df654f558c8a
Andrei Beltchikov <beltchikov@gmx.de>
2024-08-07 08:00:20 +0200

    wip

--- StockAnalyzer/DataProviders/EdgarProvider.cs
+++ StockAnalyzer/DataProviders/EdgarProvider.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace StockAnalyzer.DataProviders
 {
-    public class EdgarProvider : EdgarProviderBase, IEdgarProvider
+    public class EdgarProvider : IEdgarProvider
     {
         record Earning(DateOnly Date, List<long?> Data);
 
@@ -183,6 +183,36 @@ namespace StockAnalyzer.DataProviders
             }
 
             return earnings;
+        }
+
+        private IEnumerable<string> TableForMultipleSymbols(List<string> symbols, List<List<string>> symbolDataList)
+        {
+            List<string> uniqueDatesStringsListSorted = symbolDataList
+                            .Select(d => d[0])
+                            .SelectMany(u => u.Split("\t"))
+                            .Distinct()
+                            .OrderBy(s => s)
+                            .ToList();
+            List<string> resultList = new() { "Symbol\t" + uniqueDatesStringsListSorted.Aggregate((r, n) => r + "\t" + n) };
+
+            string dataRow = "";
+            for (int i = 0; i < symbols.Count; i++)
+            {
+                string symbol = symbols[i];
+                dataRow = symbol;
+                List<List<string>> symbolData = symbolDataList[i].Select(l => l.Split("\t").ToList()).ToList();
+
+                foreach (var date in uniqueDatesStringsListSorted)
+                {
+                    int ii = symbolData.First().IndexOf(date);
+                    string data = ii >= 0 ? symbolData.Last()[ii] : null!;
+                    dataRow += ("\t" + data);
+                }
+
+                resultList.Add(dataRow);
+            }
+
+            return resultList;
         }
     }
 }
--- StockAnalyzer/DataProviders/EdgarProviderBase.cs
+++ StockAnalyzer/DataProviders/EdgarProviderBase.cs
@@ -6,34 +6,6 @@ namespace StockAnalyzer.DataProviders
 {
     public class EdgarProviderBase
     {
-        protected IEnumerable<string> TableForMultipleSymbols(List<string> symbols, List<List<string>> symbolDataList)
-        {
-            List<string> uniqueDatesStringsListSorted = symbolDataList
-                            .Select(d => d[0])
-                            .SelectMany(u => u.Split("\t"))
-                            .Distinct()
-                            .OrderBy(s => s)
-                            .ToList();
-            List<string> resultList = new() { "Symbol\t" + uniqueDatesStringsListSorted.Aggregate((r, n) => r + "\t" + n) };
-           
-            string dataRow = "";
-            for (int i = 0; i < symbols.Count; i++)
-            {
-                string symbol = symbols[i];
-                dataRow = symbol;
-                List<List<string>> symbolData = symbolDataList[i].Select(l => l.Split("\t").ToList()).ToList();
-
-                foreach (var date in uniqueDatesStringsListSorted)
-                {
-                    int ii = symbolData.First().IndexOf(date);
-                    string data = ii >= 0 ? symbolData.Last()[ii] : null!;
-                    dataRow += ("\t" + data);
-                }
-
-                resultList.Add(dataRow);
-            }
-
-            return resultList;
-        }
+       
     }
 }
