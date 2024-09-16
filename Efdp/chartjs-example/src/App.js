551477847c4248dd65174e4eba77c55144404f1a
Andrei Beltchikov <beltchikov@gmx.de>
2024-09-16 18:14:31 +0200

    Adds chartjs-example react project

---
+++ Efdp/chartjs-example/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ChartComponent from './ChartComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chart.js in React</h1>
+      <ChartComponent />
+    </div>
+  );
+}
+
+export default App;
