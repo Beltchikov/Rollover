711747981f95d3adda516db6239177bf1bc00de9
Andrei Beltchikov <beltchikov@gmx.de>
2024-09-17 17:30:38 +0200

    wip

--- Efdp/efdp-ui-prototype/src/Banner.js
+++ Efdp/efdp-ui-prototype/src/Banner.js
@@ -3,7 +3,11 @@ import React from 'react';
 import './Banner.css';
 
 function Banner() {
-  return <div className="Banner">Essential Finance Data Portal</div>;
+  // return <div className="Banner">Essential Finance Data Portal</div>;
+  return <div className="Banner">
+    {/* <image src="../public/EfdpTitle.png"></image> */}
+    <img src={`${process.env.PUBLIC_URL}/EfdpTitle.png`} alt="Essential Finance Data Portal" className="BannerImage" />
+    </div>;
 }
 
 export default Banner;
