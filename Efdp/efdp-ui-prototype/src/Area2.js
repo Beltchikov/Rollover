7c0eec79a27134421b56af4aa5a58c54e009370b
Andrei Beltchikov <beltchikov@gmx.de>
2024-09-17 17:10:06 +0200

    wip

---
+++ Efdp/efdp-ui-prototype/src/Area2.js
@@ -0,0 +1,13 @@
+// Area2.js
+import React from 'react';
+import './MainContent.css';
+
+function Area2() {
+  return (
+    <div className="Area2">
+      <div className="Area2">Area 2</div>
+    </div>
+  );
+}
+
+export default Area2;
--- Efdp/efdp-ui-prototype/src/MainContent.js
+++ Efdp/efdp-ui-prototype/src/MainContent.js
@@ -2,12 +2,13 @@
 import React from 'react';
 import './MainContent.css';
 import Area1 from './Area1';
+import Area2 from './Area2';
 
 function MainContent() {
   return (
     <div className="MainContent">
       <Area1></Area1>
-      <div className="Area2"></div>
+      <Area2></Area2>
       <div className="Area3">Area 3</div>
     </div>
   );
