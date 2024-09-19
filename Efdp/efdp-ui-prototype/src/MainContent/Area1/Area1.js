5151e59a4d5f23f7915c99065aa1166734c5a9fb
Andrei Beltchikov <beltchikov@gmx.de>
2024-09-19 17:54:16 +0200

    wip

---
+++ Efdp/efdp-ui-prototype/src/MainContent/Area1/Area1.js
@@ -0,0 +1,13 @@
+// Area1.js
+import React from 'react';
+import './Area1.css';
+
+function Area1() {
+  return (
+    <div className="Area1">
+      <div className="Area1">Area 1</div>
+    </div>
+  );
+}
+
+export default Area1;
