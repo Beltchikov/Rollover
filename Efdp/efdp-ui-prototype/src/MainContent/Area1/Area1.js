ce69b4e727abdc67e6007eba339f4e740af0a6be
Andrei Beltchikov <beltchikov@gmx.de>
2024-09-20 10:10:38 +0200

    wip

--- Efdp/efdp-ui-prototype/src/MainContent/Area1/Area1.js
+++ Efdp/efdp-ui-prototype/src/MainContent/Area1/Area1.js
@@ -2,13 +2,13 @@
 import React from 'react';
 import './Area1.css';
 import SymbolsInput from './SymbolsInput';
-import EfdpChart from '../Chart/EfdpChart'
+import EfdpChart from '../Chart/EfdpChart'; // Assuming you have this component
 
 function Area1() {
   return (
     <div className="Area1">
-      <EfdpChart></EfdpChart>
       <SymbolsInput />
+      <EfdpChart />
     </div>
   );
 }
