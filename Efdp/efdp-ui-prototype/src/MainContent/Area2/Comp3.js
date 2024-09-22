99a5c75a5a2fc58651593508f706a27cc646e275
Andrei Beltchikov <beltchikov@gmx.de>
2024-09-22 18:12:49 +0200

    wip

---
+++ Efdp/efdp-ui-prototype/src/MainContent/Area2/Comp3.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Comp3() {
+  const Comp3Style = {
+    flexGrow:1,
+    flexBasis: '20%',
+    backgroundColor: 'gainsboro',
+    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)'
+  };
+
+  return (
+    <div style={Comp3Style}>
+    </div>
+  );
+}
+
+export default Comp3;
