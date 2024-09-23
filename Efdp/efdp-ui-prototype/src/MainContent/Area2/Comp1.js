21464c5af7077e728d6e4b77acafbed819a044c3
Andrei Beltchikov <beltchikov@gmx.de>
2024-09-23 09:16:34 +0200

    wip

--- Efdp/efdp-ui-prototype/src/MainContent/Area2/Comp1.js
+++ Efdp/efdp-ui-prototype/src/MainContent/Area2/Comp1.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Comp1() {
   const Comp1Style = {
     flexGrow: 1,
     flexBasis: '10%',
     backgroundColor: 'gainsboro',
-    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)'
+    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
+    display:'flex',
+    flexDirection:'column'
   };
 
   const SymbolsInputLabel = {
@@ -14,11 +16,50 @@ function Comp1() {
     fontSize: '12px'
   };
 
+  const SymbolsInput = {
+    textAlign: 'left',
+    fontSize: '12px',
+    backgroundColor: 'white',
+    height:'100%',
+    border: '2px dashed black',
+    whiteSpace: 'pre-wrap'
+  };
+
+  const [inputText, setInputText] = useState('NVDA\nMSFT\nGOOG');
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    const text = event.dataTransfer.getData('text/plain');
+    if (text) {
+      setInputText(text); // Replace the content with dropped text
+    }
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const handlePaste = (event) => {
+    const pastedText = event.clipboardData.getData('text');
+    if (pastedText) {
+      setInputText(pastedText); // Replace the content with pasted text
+    }
+  };
+
   return (
     <div style={Comp1Style}>
       <label style={SymbolsInputLabel}>
-        Drag and drop or paste your stock symbols below - one symbol per line:
+        Drag and drop or paste stock symbols below:
       </label>
+      <div
+        style={SymbolsInput}
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
+        // contentEditable={false} 
+        onPaste={handlePaste}
+      >
+        {inputText ? inputText : ""}
+      </div>
     </div>
   );
 }
