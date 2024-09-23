3a14e02a2d0f51c4401cda339bf9fbb0ee59328d
Andrei Beltchikov <beltchikov@gmx.de>
2024-09-23 09:04:24 +0200

    wip

--- Efdp/efdp-ui-prototype/src/MainContent/Area2/Comp1.js
+++ Efdp/efdp-ui-prototype/src/MainContent/Area2/Comp1.js
@@ -2,15 +2,23 @@ import React from 'react';
 
 function Comp1() {
   const Comp1Style = {
-    flexGrow:1,
+    flexGrow: 1,
     flexBasis: '10%',
     backgroundColor: 'gainsboro',
     boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)'
   };
 
+  const SymbolsInputLabel = {
+    display: 'block',
+    textAlign: 'left',
+    fontSize: '12px'
+  };
+
   return (
     <div style={Comp1Style}>
-        {/* &nbsp; */}
+      <label style={SymbolsInputLabel}>
+        Drag and drop or paste your stock symbols below - one symbol per line:
+      </label>
     </div>
   );
 }
