27436dc1c5149f5d7a8ebb5d11b3bb9f7f636a87
Andrei Beltchikov <beltchikov@gmx.de>
2024-09-26 08:06:53 +0200

    wip

--- Efdp/efdp-ui-prototype/src/MainContent/Area3/Area3.js
+++ Efdp/efdp-ui-prototype/src/MainContent/Area3/Area3.js
@@ -1,11 +1,15 @@
 // Area3.js
 import React from 'react';
 import './Area3.css';
+import EfdpChart from '../Chart/EfdpChart';
+import AdTeaser from '../AdTeaser';
 
 function Area3() {
   return (
     <div className="Area3">
-      <div className="Area3">Area 3</div>
+      <AdTeaser />
+      <span style={{ width: '10px' }} />
+      {/* <EfdpChart title='Long-Term Debt to Earnings Ratio' /> */}
     </div>
   );
 }
