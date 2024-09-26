e1db50959ec4bd2d51ead1cbcd03c418ed95d109
Andrei Beltchikov <beltchikov@gmx.de>
2024-09-26 16:19:34 +0200

    wip

--- Efdp/efdp-ui-prototype/src/MainContent/Area1/Area1.js
+++ Efdp/efdp-ui-prototype/src/MainContent/Area1/Area1.js
@@ -5,23 +5,33 @@ import AdTeaser from '../AdTeaser';
 
 function Area1() {
   const area1Style = {
-    flexBasis:'34%',
     display: 'flex',
     justifyContent: 'flex-start',
     backgroundColor: '#d1e7d1',
     padding: '10px',
     marginBottom: '10px',
+    gap: '10px',  // Use gap for spacing instead of individual spans
+  };
+
+  const chartStyle = {
+    width: '35%',  // Fixed 35% width for the charts
+  };
+
+  const adTeaserStyle = {
+    flexGrow: 1,  // Let AdTeaser take the remaining space
   };
 
   return (
     <div style={area1Style}>
+      {/* SymbolsInput will have auto width */}
       <SymbolsInput />
-      <span style={{ width: '10px' }} />
-      <EfdpChart type={'bar'} title='Annual Growth of FCF' />
-      <span style={{ width: '10px' }} />
-      <EfdpChart title='Free Cash Flow' />
-      <span style={{ width: '10px' }} />
-      <AdTeaser />
+
+      {/* EfdpChart components with 35% width */}
+      <EfdpChart type={'bar'} title='Annual Growth of FCF' style={chartStyle} />
+      <EfdpChart title='Free Cash Flow' style={chartStyle} />
+
+      {/* AdTeaser takes the remaining space */}
+      <AdTeaser style={adTeaserStyle} />
     </div>
   );
 }
--- Efdp/efdp-ui-prototype/src/MainContent/Area1/SymbolsInput.js
+++ Efdp/efdp-ui-prototype/src/MainContent/Area1/SymbolsInput.js
@@ -2,12 +2,10 @@ import React, { useState } from 'react';
 
 function SymbolsInput() {
   const SymbolsInputStyle = {
-    flexGrow: 1,
-    flexBasis: '10%',
     backgroundColor: 'gainsboro',
     boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
-    display:'flex',
-    flexDirection:'column'
+    display: 'flex',
+    flexDirection: 'column'
   };
 
   const SymbolsInputLabel = {
@@ -20,7 +18,7 @@ function SymbolsInput() {
     textAlign: 'left',
     fontSize: '12px',
     backgroundColor: 'white',
-    height:'100%',
+    flex: 1,
     border: '2px dashed black',
     whiteSpace: 'pre-wrap'
   };
@@ -51,7 +49,6 @@ function SymbolsInput() {
       <label style={SymbolsInputLabel}>
         Drag and drop or paste stock symbols below:
       </label>
-      <div style={{height:'10px'}}></div>
       <div
         style={SymbolsInput}
         onDrop={handleDrop}
