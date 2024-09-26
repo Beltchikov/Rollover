a8d8aa4950095c2434890ae84f7bc0ce3c18ade8
Andrei Beltchikov <beltchikov@gmx.de>
2024-09-26 09:15:43 +0200

    wip

--- Efdp/efdp-ui-prototype/src/Footer.js
+++ Efdp/efdp-ui-prototype/src/Footer.js
@@ -1,9 +1,17 @@
 // Footer.js
 import React from 'react';
-import './Footer.css';
 
 function Footer() {
-  return <div className="Footer">Footer</div>;
+  const footerStyle = {
+    backgroundColor: '#225f91',
+    height: '10%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
+  };
+
+  return <div style={footerStyle}>Footer</div>;
 }
 
 export default Footer;
