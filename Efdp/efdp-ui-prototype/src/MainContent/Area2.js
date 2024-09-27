85067fd232b3373e4d7dedc2525397711ca61516
Andrei Beltchikov <beltchikov@gmx.de>
2024-09-27 09:42:15 +0200

    wip

--- Efdp/efdp-ui-prototype/src/MainContent/Area2.js
+++ Efdp/efdp-ui-prototype/src/MainContent/Area2.js
@@ -1,10 +1,19 @@
-// Area2.js
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import EfdpChart from './Chart/EfdpChart';
+import { fetchRetainedEarnings } from '../store';
 
 function Area2() {
-  const { dataFcfCapExRatio, dataRetainedEarnings, dataGpm } = useSelector((state) => state.global.area2);
+  const dispatch = useDispatch();
+
+  // Fetch retained earnings data when the component is mounted
+  useEffect(() => {
+    dispatch(fetchRetainedEarnings());
+  }, [dispatch]);
+
+  const { dataFcfCapExRatio, dataRetainedEarnings, dataGpm } = useSelector(
+    (state) => state.global.area2
+  );
 
   const area2Style = {
     flexBasis: '33%',
@@ -13,13 +22,18 @@ function Area2() {
     backgroundColor: '#d1e7d1',
     padding: '10px',
     marginBottom: '10px',
+    gap: '10px'
   };
 
   return (
     <div style={area2Style}>
-      <EfdpChart title='FCF-CapEx-Ratio' data={dataFcfCapExRatio} />
-      <EfdpChart title='Retained Earnings' data={dataRetainedEarnings} />
-      <EfdpChart title='Gross Profit Margin' data={dataGpm} />
+      <EfdpChart title="FCF-CapEx-Ratio" data={dataFcfCapExRatio} />
+      {dataRetainedEarnings ? (
+        <EfdpChart title="Retained Earnings" data={dataRetainedEarnings} />
+      ) : (
+        <div style={area2Style}>Loading...</div>
+      )}
+      <EfdpChart title="Gross Profit Margin" data={dataGpm} />
     </div>
   );
 }
