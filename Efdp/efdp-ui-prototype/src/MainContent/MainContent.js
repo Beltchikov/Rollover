c3c57670cb3ee828aff557dd6e34dd1dd37ce75e
Andrei Beltchikov <beltchikov@gmx.de>
2024-09-27 07:52:03 +0200

    wip

--- Efdp/efdp-ui-prototype/src/MainContent/MainContent.js
+++ Efdp/efdp-ui-prototype/src/MainContent/MainContent.js
@@ -1,16 +1,23 @@
 // MainContent.js
 import React from 'react';
-import './MainContent.css';
 import Area1 from './Area1/Area1';
 import Area2 from './Area2/Area2';
 import Area3 from './Area3/Area3';
 
 function MainContent() {
+  const mainContentStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: 1,
+    backgroundColor: '#e0f0d9',
+    padding: '10px',
+  };
+
   return (
-    <div className="MainContent">
-      <Area1></Area1>
-      <Area2></Area2>
-      <Area3></Area3>
+    <div className="MainContent" style={mainContentStyle}>
+      <Area1 />
+      <Area2 />
+      <Area3 />
     </div>
   );
 }
