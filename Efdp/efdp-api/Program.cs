80ebe9ba33af24957fb497dd71859844ab3edff1
Andrei Beltchikov <beltchikov@gmx.de>
2024-09-28 10:05:26 +0200

    adds block builder.Services.AddCors but commented

--- Efdp/efdp-api/Program.cs
+++ Efdp/efdp-api/Program.cs
@@ -4,6 +4,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy("AllowAllOrigins", builder =>
+//     {
+//         builder.AllowAnyOrigin()
+//                .AllowAnyMethod()
+//                .AllowAnyHeader();
+//     });
+// });
 
 var app = builder.Build();
 
