4cdf358757942a2cc1613a3238a135c613fe85b0
Andrei Beltchikov <beltchikov@gmx.de>
2024-10-03 10:07:20 +0200

    wip

--- Efdp/efdp-api/Program.cs
+++ Efdp/efdp-api/Program.cs
@@ -40,98 +40,66 @@ internal class Program
         .WithName("GetBalanceSheetStatement")
         .WithOpenApi();
 
-        app.MapGet("/balance-sheet-statement-mock", () =>
-        {
-            // Define the path to the MockResponses directory
-            string mockDirectory = Path.Combine(Directory.GetCurrentDirectory(), "MockResponses", "balance-sheet-statement");
-
-            // Check if the directory exists
-            if (!Directory.Exists(mockDirectory))
-            {
-                return Results.NotFound("MockResponses directory not found.");
-            }
-
-            // Prepare a dictionary to hold the mock responses
-            var balanceSheetResponseDict = new Dictionary<string, List<BalanceSheetStatement>>();
-
-            // Get all the files in the directory (assuming .json files)
-            var files = Directory.GetFiles(mockDirectory, "*.json");
-
-            foreach (var file in files)
-            {
-                // Extract the symbol from the file name (assuming the file name follows a certain pattern)
-                var fileName = Path.GetFileNameWithoutExtension(file); // e.g., "AAPL.json" -> "AAPL"
-                string jsonContent = File.ReadAllText(file); // Read the file content
-
-                // Deserialize the content into a list of BalanceSheetStatement objects
-                var balanceSheetStatements = JsonSerializer.Deserialize<List<BalanceSheetStatement>>(jsonContent);
-
-                // Add the deserialized content to the response dictionary
-                if (balanceSheetStatements != null)
-                {
-                    balanceSheetResponseDict[fileName] = balanceSheetStatements;
-                }
-            }
-
-            // Return the response as JSON
-            return Results.Json(balanceSheetResponseDict);
-        })
-        .WithName("GetBalanceSheetStatementMock")
-        .WithOpenApi(); ;
-
         app.MapGet("/cash-flow-statement", async (HttpClient httpClient, string[] stockSymbols) =>
-        {
-            string apiKey = "14e7a22ed6110f130afa41af05599bb6";
-            string baseUrl = "https://financialmodelingprep.com/api/v3/cash-flow-statement/";
+       {
+           string apiKey = "14e7a22ed6110f130afa41af05599bb6";
+           string baseUrl = "https://financialmodelingprep.com/api/v3/cash-flow-statement/";
 
-            Dictionary<string, string> cashFlowResponseDict = await FetchFmpResponses(httpClient, stockSymbols, baseUrl, apiKey);
-            var cashFlowStatementDict = DeserializeFmpResponses<CashFlowStatement>(cashFlowResponseDict);
+           Dictionary<string, string> cashFlowResponseDict = await FetchFmpResponses(httpClient, stockSymbols, baseUrl, apiKey);
+           var cashFlowStatementDict = DeserializeFmpResponses<CashFlowStatement>(cashFlowResponseDict);
 
-            return Results.Ok(cashFlowStatementDict);
-        })
-        .WithName("GetCashFlowStatement")
+           return Results.Ok(cashFlowStatementDict);
+       })
+       .WithName("GetCashFlowStatement")
+       .WithOpenApi();
+
+        app.MapGet("/balance-sheet-statement-mock", () => GetMockStatements<BalanceSheetStatement>("balance-sheet-statement"))
+        .WithName("GetBalanceSheetStatementMock")
         .WithOpenApi();
-
-        app.MapGet("/cash-flow-statement-mock", () =>
-        {
-            // Define the path to the MockResponses directory
-            string mockDirectory = Path.Combine(Directory.GetCurrentDirectory(), "MockResponses", "cash-flow-statement");
-
-            // Check if the directory exists
-            if (!Directory.Exists(mockDirectory))
-            {
-                return Results.NotFound("MockResponses directory not found.");
-            }
-
-            // Prepare a dictionary to hold the mock responses
-            var cashFlowResponseDict = new Dictionary<string, List<CashFlowStatement>>();
-
-            // Get all the files in the directory (assuming .json files)
-            var files = Directory.GetFiles(mockDirectory, "*.json");
-
-            foreach (var file in files)
-            {
-                // Extract the symbol from the file name (assuming the file name follows a certain pattern)
-                var fileName = Path.GetFileNameWithoutExtension(file); // e.g., "AAPL.json" -> "AAPL"
-                string jsonContent = File.ReadAllText(file); // Read the file content
-
-                // Deserialize the content into a list of BalanceSheetStatement objects
-                var cashFlowStatements = JsonSerializer.Deserialize<List<CashFlowStatement>>(jsonContent);
-
-                // Add the deserialized content to the response dictionary
-                if (cashFlowStatements != null)
-                {
-                    cashFlowResponseDict[fileName] = cashFlowStatements;
-                }
-            }
-
-            // Return the response as JSON
-            return Results.Json(cashFlowResponseDict);
-        })
-       .WithName("GetCashFlowStatementMock")
-       .WithOpenApi(); ;
-
+        
+        app.MapGet("/cash-flow-statement-mock", () => GetMockStatements<CashFlowStatement>("cash-flow-statement"))
+        .WithName("GetCashFlowStatementMock")
+        .WithOpenApi();
+       
         app.Run();
+    }
+
+    // Helper method to read mock responses from a directory
+    static IResult GetMockStatements<T>(string statementType)
+    {
+        // Define the path to the MockResponses directory
+        string mockDirectory = Path.Combine(Directory.GetCurrentDirectory(), "MockResponses", statementType);
+
+        // Check if the directory exists
+        if (!Directory.Exists(mockDirectory))
+        {
+            return Results.NotFound($"{statementType} mock responses directory not found.");
+        }
+
+        // Prepare a dictionary to hold the mock responses
+        var responseDict = new Dictionary<string, List<T>>();
+
+        // Get all the files in the directory (assuming .json files)
+        var files = Directory.GetFiles(mockDirectory, "*.json");
+
+        foreach (var file in files)
+        {
+            // Extract the symbol from the file name (assuming the file name follows a certain pattern)
+            var fileName = Path.GetFileNameWithoutExtension(file); // e.g., "AAPL.json" -> "AAPL"
+            string jsonContent = File.ReadAllText(file); // Read the file content
+
+            // Deserialize the content into a list of statements
+            var statements = JsonSerializer.Deserialize<List<T>>(jsonContent);
+
+            // Add the deserialized content to the response dictionary
+            if (statements != null)
+            {
+                responseDict[fileName] = statements;
+            }
+        }
+
+        // Return the response as JSON
+        return Results.Json(responseDict);
     }
 
 
