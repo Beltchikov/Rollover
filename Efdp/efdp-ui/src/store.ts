4dd4712315b8a92fc9f8fe3c7392e6de73033676
Andrei Beltchikov <beltchikov@gmx.de>
2024-10-04 10:35:57 +0200

    wip

--- Efdp/efdp-ui/src/store.ts
+++ Efdp/efdp-ui/src/store.ts
@@ -31,7 +31,7 @@ interface GlobalState {
     cashFlowStatementDict: Record<string, any>;
     cashFlowStatementUpdated: Date;
     balanceSheetStatementDict: Record<string, any>;
-    balanceSheetStatement: Date;
+    balanceSheetStatementUpdated: Date;
     area1: {
         dataCagrFcf: AreaState;
         dataFcf: AreaState | null;  // Could be null initially
@@ -54,7 +54,7 @@ const initialState: GlobalState = {
     cashFlowStatementDict: {},
     cashFlowStatementUpdated: new Date(),
     balanceSheetStatementDict: {},
-    balanceSheetStatement: new Date(),
+    balanceSheetStatementUpdated: new Date(),
     area1: {
         dataCagrFcf: {
             labels: ['2009-09-26', '2009-12-31', '2010-06-30', '2010-09-25', '2010-12-31'],
