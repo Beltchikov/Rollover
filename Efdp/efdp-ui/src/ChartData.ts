92ff4bb4a67b17ecb1299e52cb08685fb032debc
Andrei Beltchikov <beltchikov@gmx.de>
2024-10-06 08:29:31 +0200

    wip

--- Efdp/efdp-ui/src/ChartData.ts
+++ Efdp/efdp-ui/src/ChartData.ts
@@ -12,3 +12,41 @@ export interface IChartDataset {
     hidden: boolean;
     borderWidth: number;
 }
+
+export class ChartDataset implements IChartDataset {
+    label: string;
+    data: (number | null)[];
+    borderColor: string;
+    backgroundColor: string;
+    yAxisID: string;
+    hidden: boolean;
+    borderWidth: number;
+
+    constructor(
+        label: string, 
+        data: (number | null)[], 
+        borderColor: string, 
+        backgroundColor: string, 
+        yAxisID: string, 
+        hidden: boolean, 
+        borderWidth: number
+    ) {
+        this.label = label;
+        this.data = data;
+        this.borderColor = borderColor;
+        this.backgroundColor = backgroundColor;
+        this.yAxisID = yAxisID;
+        this.hidden = hidden;
+        this.borderWidth = borderWidth;
+    }
+}
+
+export class ChartData implements IChartData {
+    labels: string[];
+    datasets: ChartDataset[];
+
+    constructor(labels: string[], datasets: ChartDataset[]) {
+        this.labels = labels;
+        this.datasets = datasets;
+    }
+}
