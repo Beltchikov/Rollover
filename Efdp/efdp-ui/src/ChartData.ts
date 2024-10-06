c0540a2de87dbc22ab433554e49f514c79367b35
Andrei Beltchikov <beltchikov@gmx.de>
2024-10-06 07:58:39 +0200

    wip

--- Efdp/efdp-ui/src/ChartData.ts
+++ Efdp/efdp-ui/src/ChartData.ts
@@ -1,6 +1,14 @@
-import { ChartDataset } from "./ChartDataset";
-
 export interface ChartData {
     labels: string[];
     datasets: ChartDataset[];
+}
+
+export interface ChartDataset {
+    label: string;
+    data: (number | null)[]; 
+    borderColor: string;
+    backgroundColor: string;
+    yAxisID: string;
+    hidden: boolean;
+    borderWidth: number;
 }
