e58911c0e3c83f9d66201efd6cedb5de3f2aea1d
Andrei Beltchikov <beltchikov@gmx.de>
2024-10-11 09:27:44 +0200

    removes efdp folder

--- Efdp/efdp-ui/src/config.js
+++
@@ -1,4 +0,0 @@
-export const EFDP_API_BASE_URL = process.env.REACT_APP_EFDP_API_BASE_URL || '';
-export const USE_MOCK_RESPONSES = process.env.REACT_APP_USE_MOCK_RESPONSES === 'true';
-export const YEARS_AGO = 10;
-
